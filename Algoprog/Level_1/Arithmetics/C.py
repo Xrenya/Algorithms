7b83b23f9e60b5b0367991425a0c479aeaa1e238
Rinat <51479797+Xrenya@users.noreply.github.com>
2020-11-10 11:13:28 +0900

    Create C.py

---
+++ Algoprog/Level_1/Arithmetics/C.py
@@ -0,0 +1,7 @@
+number = int(input())
+
+def text(number):
+    print(f"The next number for the number {number} is {number+1}.")
+    print(f"The previous number for the number {number} is {number-1}.")
+
+text(number)
