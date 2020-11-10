84b8527e7d46f6385046a5f9994505e6bfc0bad6
Rinat <51479797+Xrenya@users.noreply.github.com>
2020-11-10 11:00:18 +0900

    quotient

--- Algoprog/Level_1/Arithmetics/A.py
+++ Algoprog/Level_1/Arithmetics/A.py
@@ -1,7 +1,7 @@
 num_pers = int(input())
 num_objs = int(input())
 
-def div(num_pers, num_objs):
+def quotient(num_pers, num_objs):
     return num_objs // num_pers
 
-print(div(num_pers, num_objs))
+print(quotient(num_pers, num_objs))
