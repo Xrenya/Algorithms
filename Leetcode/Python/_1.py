a452da48f68604b5ae2aeb6d278ca79b16de0d8f
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-09 20:57:55 +0900

    Update _1.py

--- Leetcode/Python/_1.py
+++ Leetcode/Python/_1.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Time complexity O(n)
         hashMap = {} # value: index
         
         for i in range(len(nums)):
@@ -7,3 +8,11 @@ class Solution:
                 return [hashMap.get(target - nums[i]), i]
             else: 
                 hashMap[nums[i]] = i
+                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # Time complexity O(n^2)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i, j]
