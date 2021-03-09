f228aa7e647a7560de656f0bd57bd732aa883647
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-09 21:03:57 +0900

    Update _1.py

--- Leetcode/Python/_1.py
+++ Leetcode/Python/_1.py
@@ -5,14 +5,15 @@ class Solution:
         
         for i in range(len(nums)):
             if hashMap.get(target - nums[i]) is not None:
+                # if the differene is in the dictionary it will return the position
                 return [hashMap.get(target - nums[i]), i]
             else: 
-                hashMap[nums[i]] = i
+                # if the difference is not in the dictionary it will be added
+                hashMap[nums[i]] = i 
                 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def bruteForceTwoSum(self, nums: List[int], target: int) -> List[int]:
     # Time complexity O(n^2)
-        for i in range(len(nums)):
+        for i in range(len(nums)-1): # not including the last element
             for j in range(i+1, len(nums)):
                 if target == nums[i] + nums[j]:
                     return [i, j]
