08030ca0d37cada8b9f4f71a6aff78e87bab6290
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-11 12:36:41 +0900

    Create _4.py

---
+++ Leetcode/Python/_4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """Merge two array, sort it and jsut find the median of a sorted array
+        """
+        merge = nums1 + nums2
+        merge = sorted(merge)
+        if len(merge) % 2 != 0:
+            return merge[len(merge) // 2]
+        else:
+            return (merge[len(merge) // 2] + merge[len(merge) // 2 - 1])/2
+                
