3d491273a24df42e484f7b25b877fd88473e5ef9
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-16 00:05:31 +0900

    Update _1603.py

--- Leetcode/Python/_1603.py
+++ Leetcode/Python/_1603.py
@@ -6,3 +6,12 @@ class ParkingSystem:
     def addCar(self, carType: int) -> bool:
         self.count[carType] -= 1
         return self.count[carType] >= 0
+    
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.count = {1: big, 2: medium, 3: small}
+        
+    def addCar(self, carType: int) -> bool:
+        self.count[carType] -= 1
+        return self.count[carType] >= 0
