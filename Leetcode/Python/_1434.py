1066cfc0aa9ddc35cefcd527457e21d0d865336e
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-15 11:47:42 +0900

    Create _1434.py

---
+++ Leetcode/Python/_1434.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        # Find the maximum in the array
+        maximum = 0
+        for candy in candies:
+            if candy > maximum:
+                maximum = candy
+        arr = []
+        # If the new value is higher than the maximum append True, else it is False
+        for candy in candies:
+            if (candy + extraCandies) >= maximum:
+                arr.append(True)
+            else:
+                arr.append(False)
+        return arr
