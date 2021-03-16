81af2f23735efe1a8b15d4faf70c8af50ab8afea
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-16 23:17:04 +0900

    Update _1365.py

--- Leetcode/Python/_1365.py
+++ Leetcode/Python/_1365.py
@@ -9,3 +9,26 @@ class Solution:
         for i in range(1, 102):
             count[i] += count[i-1]
         return [count[num] for num in nums]
+
+def insertationSort(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+        while (i>-1) and (array[i]>key):
+            array[i+1] = array[i]
+            i -= 1
+        array[i+1] = key
+    return array
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        array = nums
+        temp = insertationSort(nums.copy())
+        mapping = {}
+        result = []
+        for i in range(len(temp)):
+            if temp[i] not in mapping:
+                mapping[temp[i]] = i
+        for i in range(len(nums)):
+            result.append(mapping[nums[i]])
+        return result
