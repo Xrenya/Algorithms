a6d7adeeaf17223a250ae7f2e205190b9fc3b298
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-17 00:00:56 +0900

    Create _ 1342.py

---
+++ Leetcode/Python/_ 1342.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        # Do a bitwise opertaion instead of math calculations
+        # 14 -> 1110: step = 0
+        # >>= 1 bitwise shift to left 111 -> 7 : step += 1
+        # >>= 1 bitwise shift to left 11 -> 3 : step += 2
+        # >>= 1 bitwise shift to left 1 -> 1 : step += 2
+        # >>= 1 bitwise shift to left 0 -> 0 : step += 2
+        
+        steps = num == 0
+        while num > 0:
+            steps += 1 + (num & 1)
+            num >>= 1
+            print(num)
+            print(steps)
+        return steps - 1
+      
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        steps = 0
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            steps += 1
+        return steps
+        
