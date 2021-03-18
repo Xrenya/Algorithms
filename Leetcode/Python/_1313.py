379a451521886ca8869ebee084e523ca82f31c9e
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-18 13:57:31 +0900

    Update _1313.py

--- Leetcode/Python/_1313.py
+++ Leetcode/Python/_1313.py
@@ -8,6 +8,6 @@ class Solution:
  class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
         array = []
-        for i in range(0,len(nums)-1,2): 
+        for i in range(0,len(nums),2): 
             array += nums[i]*[nums[i+1]]
         return array
