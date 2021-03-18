e9e504c4951be0e2ab2c551849ddde229f5916c2
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-18 13:29:02 +0900

    Update _1678.py

--- Leetcode/Python/_1678.py
+++ Leetcode/Python/_1678.py
@@ -2,19 +2,33 @@ class Solution:
     def interpret(self, command: str) -> str:
         return command.replace("(al)", "al").replace("()", "o")
         
- class Solution:
+class Solution:
     def interpret(self, command: str) -> str:
         string = str()
-        for idx, c in enumerate(command):
-            if c == 'G':
-                string += c
-            elif c == '(':
+        for idx, char in enumerate(command):
+            if char == 'G':
+                string += char
+            elif char == '(':
                 if command[idx+1] == ')':
                     string += 'o'
                 elif command[idx+1] == 'a':
                     string += 'al'
                 else:
                     pass
+            else:
+                pass
+        return string
+   
+class Solution:
+    def interpret(self, command: str) -> str:
+        string = str()
+        for idx, char in enumerate(command):
+            if char == 'G':
+                string += char
+            elif char == '(' and command[idx+1] == ')':
+                string += 'o'
+            elif char == '(' and command[idx+1] == 'a':
+                string += 'al'
             else:
                 pass
         return string
