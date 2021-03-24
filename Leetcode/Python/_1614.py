e23ca7080529ee2f63740293e016dcdb6af87349
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-24 14:10:59 +0900

    Update _1614.py

--- Leetcode/Python/_1614.py
+++ Leetcode/Python/_1614.py
@@ -10,3 +10,28 @@ class Solution:
             if maxDepth < depth:
                 maxDepth = depth
         return maxDepth
+    
+    
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        hashMap = {'(' : 1, ')' : -1}
+        maxDepth = 0
+        depth = 0
+        for string in s:
+            depth += hashMap.get(string, 0)
+            if depth > maxDepth:
+                maxDepth = depth
+        return maxDepth
+    
+    
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        hashMap = {'(' : 1, ')' : -1}
+        maxDepth = 0
+        depth = 0
+        for string in s:
+            if string is "(" or string is ")":
+                depth += hashMap[string]
+                if depth > maxDepth:
+                    maxDepth = depth
+        return maxDepth
