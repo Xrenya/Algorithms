2b7493064e56a518ac3bc481387fd80f0a37d08d
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-24 14:42:30 +0900

    Update _1684.py

--- Leetcode/Python/_1684.py
+++ Leetcode/Python/_1684.py
@@ -13,4 +13,38 @@ class Solution:
                 counter += 1
             inc = 0
         return counter
-            
+ 
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        counter = 0
+        hashMap = {}
+        
+        for letter in allowed:
+            if letter not in hashMap:
+                hashMap[letter] = 1
+                
+        for word in words:
+            consistent = 1
+            for letter in word:
+                if letter not in hashMap:
+                    consistent = 0
+                    
+            if consistent == 1:
+                counter += 1
+                
+        return counter
+ 
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        counter = len(words)
+        hashMap = dict()
+        for letter in allowed:
+            if letter not in hashMap:
+                hashMap[letter] = 1 
+        
+        for word in words:
+            for letter in word:
+                if not letter in hashMap:
+                    counter -= 1
+                    break
+        return counter
