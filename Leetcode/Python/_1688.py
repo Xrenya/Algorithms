8c3e310af304ad08f9bcf43bb968505b6045df04
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-25 17:22:30 +0900

    Create _1688.py

---
+++ Leetcode/Python/_1688.py
@@ -0,0 +1,43 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n != 1:
+            if n % 2 == 0:
+                matches += n // 2
+                n /= 2
+            else:
+                matches += (n - 1) // 2 
+                n = (n - 1) / 2 + 1
+        return int(matches)
+      
+      
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n != 1:
+            if n % 2 == 0:
+                matches += n // 2
+                n //= 2
+            else:
+                matches += (n - 1) // 2 
+                n = (n - 1) // 2 + 1
+        return matches
+                
+class Solution:
+    def calc(self, n, matches):
+        if (n == 1):
+            return 0
+
+        if n % 2 == 0:
+            matches = n // 2
+            n //= 2
+            return (matches + self.calc(n, matches))
+
+        if n % 2 != 0:
+            matches = (n-1) // 2
+            n = (n-1) // 2 + 1
+            return matches + self.calc(n, matches)
+        
+    def numberOfMatches(self, n: int) -> int:
+        return self.calc(n, 0)
+                
