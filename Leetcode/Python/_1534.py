c7cddca080031d77456a81fd28fbc633cd1b3e06
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-03-27 23:16:42 +0900

    Update _1534.py

--- Leetcode/Python/_1534.py
+++ Leetcode/Python/_1534.py
@@ -13,3 +13,21 @@ class Solution:
                         else:
                             pass
         return counter
+    
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        
+        size = len(arr)
+        counter = 0
+        
+        for i in range(size-2):
+            for j in range(i+1, size-1):
+                for k in range(j+1, size):
+                    
+                    ok_a = abs(arr[i] - arr[j]) <= a
+                    ok_b = abs(arr[j] - arr[k]) <= b
+                    ok_c = abs(arr[i] - arr[k]) <= c
+                    
+                    if all((ok_a, ok_b, ok_c)):
+                        counter += 1   
+        return counter
