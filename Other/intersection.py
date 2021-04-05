ef86e2276e46bf7dd6e6fb591db62d2572504f62
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-05 10:23:34 +0900

    Create intersection.py

---
+++ Other/intersection.py
@@ -0,0 +1,22 @@
+"""
+Даны два массива: [1, 2, 3, 2, 0] и [5, 1, 2, 7, 3, 2]
+Надо вернуть [1, 2, 2, 3] (порядок неважен)
+"""
+def intersection(l1, l2):
+    # Time complexity O(n) + O(m) 
+    hashMap = dict()
+    for element in l1:
+        if element in hashMap:
+            hashMap[element] += 1
+        else:
+            hashMap[element] = 1
+    
+    out_array = []
+    for element in l2:
+        if element in hashMap:
+            if hashMap[element] > 0:
+                out_array.append(element)
+                hashMap[element] -= 1
+    return out_array
+    
+print(intersection([1, 2, 3, 2, 0], [5, 1, 2, 7, 3, 2]))
