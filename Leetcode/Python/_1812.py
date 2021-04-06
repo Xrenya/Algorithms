2b5e67870f1c6c78aa9a4c0b3ebbc0170e2bdab4
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-06 12:45:57 +0900

    Create _1812.py

---
+++ Leetcode/Python/_1812.py
@@ -0,0 +1,9 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        hashMap = {"a":1, "b":2, "c":3, "d":4,
+                   "e":5, "f":6, "g":7, "h":8}
+        row, col = int(coordinates[1]), hashMap[coordinates[0]]
+        if (row%2 != 0 and col%2 != 0) or (row%2 == 0 and col%2 == 0):
+            return False
+        else:
+            return True
