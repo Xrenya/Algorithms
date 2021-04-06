a08f81ee50cf183327132dcd77ebba69d25ff355
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-06 12:52:21 +0900

    Update _1812.py

--- Leetcode/Python/_1812.py
+++ Leetcode/Python/_1812.py
@@ -7,3 +7,13 @@ class Solution:
             return False
         else:
             return True
+
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        hashMap = {"a":1, "b":2, "c":3, "d":4,
+                   "e":5, "f":6, "g":7, "h":8}
+        row, col = int(coordinates[1]), hashMap[coordinates[0]]
+        if row%2==col%2:
+            return False
+        else:
+            return True
