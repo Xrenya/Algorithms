fa98d77c7a640ddf018a2c485aac366a1c6f27f8
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-12 18:11:02 +0900

    Update _1207.py

--- Leetcode/Python/_1207.py
+++ Leetcode/Python/_1207.py
@@ -40,3 +40,33 @@ class Solution:
             else:
                 hashMap[num] += 1
         return len(hashMap) == len(set(hashMap.values()))
+    
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hashMap = {}
+        for num in arr:
+            if num not in hashMap:
+                hashMap[num] = 1
+            else:
+                hashMap[num] += 1
+        array = []
+        for val in hashMap.values():
+            array.append(val)
+        return len(array) == len(set(array))
+    
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hashMap = {}
+        for num in arr:
+            if num not in hashMap:
+                hashMap[num] = 1
+            else:
+                hashMap[num] += 1
+        array = []
+        for val in hashMap.values():
+            array.append(val)
+        while array:
+            num = array.pop()
+            if num in array:
+                return False
+        return True
