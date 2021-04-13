2104e30c867724cd6eaf14898affaf972a1ee705
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-13 12:55:13 +0900

    Update _977.py

--- Leetcode/Python/_977.py
+++ Leetcode/Python/_977.py
@@ -6,6 +6,21 @@ class Solution:
         r = size - 1
         while l <= r:
             if abs(nums[l]) > abs(nums[r]):
+                arrayQueue.appendleft(nums[l]*nums[l])
+                l += 1
+            else:
+                arrayQueue.appendleft(nums[r]*nums[r])
+                r -= 1
+        return arrayQueue
+    
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        arrayQueue = collections.deque()
+        size = len(nums)
+        l = 0
+        r = size - 1
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
                 arrayQueue.appendleft(nums[l]**2)
                 l += 1
             else:
