cfab5ed6fbc78ad9fd9aa586d439de3ce5b704ca
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-17 21:27:19 +0900

    Update _1122.py

--- Leetcode/Python/_1122.py
+++ Leetcode/Python/_1122.py
@@ -1,5 +1,26 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        hashMap = collections.Counter(arr1)
+        array = []
+        for num in arr2:
+            array += [num] * hashMap.pop(num)
+            
+        return array + sorted(hashMap.elements())
+    
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        hashMap = collections.Counter(arr1)
+        array = []
+        for num in arr2:
+            array += [num] * hashMap.pop(num)
+            
+        for num in sorted(hashMap.keys()):
+            array += [num] * hashMap[num]
+            
+        return array
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         hashMap = {}
         for num in arr1:
             if num not in hashMap:
