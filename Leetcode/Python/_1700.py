8dba712caba0752905e3fc021141f8690549f341
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-18 16:30:32 +0900

    Create _1700.py

---
+++ Leetcode/Python/_1700.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = 0
+        hashStudent = {0:0, 1:0}
+        hashSandwich = {0:0, 1:0}
+        
+        for student in students:
+            hashStudent[student] += 1
+        
+        for sandwich in sandwiches:
+            hashSandwich[sandwich] += 1
+                
+        flag = True 
+        while flag:
+            if students[0] != sandwiches[0]:
+                students.append(students.pop(0))
+                if hashSandwich[sandwiches[0]] > 0 and hashStudent[sandwiches[0]] == 0:
+                    flag = False
+                    break
+            else:
+                students.pop(0)
+                firstS = sandwiches.pop(0)
+                hashSandwich[firstS] -= 1
+                hashStudent[firstS] -= 1
+                if len(students) == 0:
+                    flag = False
+                    break
+        return hashStudent[0] + hashStudent[1]
