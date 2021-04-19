bbf6031949a7f8d8e2e55b7796e2009a713c790e
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-19 09:38:15 +0900

    Update _1200.py

--- Leetcode/Python/_1200.py
+++ Leetcode/Python/_1200.py
@@ -1,5 +1,21 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort() # O(nlogn)
+        history, min_diff = {}, float("inf")
+        
+        for i in range(len(arr)-1): # O(n)
+            if min_diff >= abs(arr[i] - arr[i+1]):
+                min_diff = abs(arr[i] - arr[i+1])
+                if min_diff not in history:
+                    history[min_diff] = []
+                    history[min_diff].append([arr[i], arr[i+1]])
+                else:
+                    history[min_diff].append([arr[i], arr[i+1]])
+      
+        return history[min_diff]
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr = sorted(arr)
         array = []
         minimum = +inf
