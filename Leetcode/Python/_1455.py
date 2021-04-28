26937a90ec422a867f93a864b77c03a48a947216
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-28 22:07:47 +0900

    Create _1455.py

---
+++ Leetcode/Python/_1455.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        lenght = len(searchWord)
+        for i, word in enumerate(sentence.split(" ")):
+            if searchWord == word[:lenght]:
+                return i+1
+        return -1
+      
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        for i, word in enumerate(sentence.split(" ")):
+            if word.startswith(searchWord):
+                return i+1
+        return -1
