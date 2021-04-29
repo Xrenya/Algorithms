0415e987e47cd10df053eb26a05d730a48c04234
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-04-29 20:54:41 +0900

    Create _1800.py

---
+++ Leetcode/Python/_1800.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        array = []
+        count = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                count += nums[i]
+            else:
+                array.append(count)
+                count = nums[i]
+        array.append(count)
+        return max(array)
+      
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        count = nums[0]
+        temp = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                temp += nums[i]
+            else:
+                temp = nums[i]
+            if count < temp:
+                count = temp
+        return count
+      
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        temp = 0
+        acc = 0
+        for i in range(len(nums)):
+            if i == 0 or nums[i-1] < nums[i]:
+                temp += nums[i]
+            else:
+                temp = nums[i]
+            if temp > acc:
+                acc = temp
+        return acc
