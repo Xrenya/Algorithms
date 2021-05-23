87fef8e4e23befd3fcbdf36e55370cf3221dfdd5
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-05-23 22:55:21 +0900

    Create _1598.py

---
+++ Leetcode/Python/_1598.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for log in logs:
+            if log == "./":
+                continue
+            elif log == "../":
+                count -= 1
+                if count < 0:
+                    count = 0
+            else:
+                count += 1
+        return count
+        
