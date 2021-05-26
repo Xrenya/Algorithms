2163d228c4b289c760aed8667dbaa2da684042ef
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-05-27 04:56:22 +0900

    Update _1103.py

--- Leetcode/Python/_1103.py
+++ Leetcode/Python/_1103.py
@@ -8,3 +8,20 @@ class Solution:
             give += 1
             candies -= give
         return out
+
+class Solution:
+    def distributeCandies(self, c: int, n: int) -> List[int]:
+        k = math.floor((math.sqrt(1+8*c)-1)/2/n)
+        res = [0 for i in range(n)]
+        for i in range(n):
+            res[i] = (i+1+(k-1)*(n)+i+1)*k/2
+        remain = c - sum(res)
+        i = 0
+        cur = k*n+1
+        while remain >= 0:
+            res[i] += min(remain,cur)
+            remain -= cur
+            
+            cur += 1
+            i += 1
+        return [int(i) for i in res]
