e31efea510027916e2e295e8dfb3ab386a588796
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-05 13:04:54 +0900

    Update _967.py

--- Leetcode/Python/_967.py
+++ Leetcode/Python/_967.py
@@ -1,4 +1,8 @@
 class Solution:
+    def transpose(self, A: List[List[int]]) -> List[List[int]]:
+        return [*map(list, zip(*A))]
+    
+class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         output = []
         for col in zip(*matrix):
