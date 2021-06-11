5e32e5d986e369c3345935725044c1d55798239a
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-11 16:23:41 +0900

    Update main.py

--- yandex/Lecture_3/Lecture/main.py
+++ yandex/Lecture_3/Lecture/main.py
@@ -1,1 +1,7 @@
-
+def two_sum(array, target):
+    prevnum = set()
+    for nownum in array:
+        if target - nownum in prevnum:
+            return nownum, target - nownum 
+        prevnum.add(nownum)
+    return 0, 0
