da4a71b4e8c8872cdd721aec4438c9f27e2b4d05
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-12 16:57:35 +0900

    Delete yandex/Lecture_2/Lecture/Contest directory

--- yandex/Lecture_2/Lecture/Contest/B/main.py
+++
@@ -1,6 +0,0 @@
-array_1 = set(map(int, input().split()))
-array_2 = set(map(int, input().split()))
- 
-intersection = sorted(list(array_1.intersection(array_2)))
-
-print(" ".join(map(str, intersection)))
--- yandex/Lecture_2/Lecture/Contest/D/main.py
+++
@@ -1,7 +0,0 @@
-with open('input.txt') as f:
-    output = set()
-    for line in f.readlines():
-        output |= set(line.split()) # Union
-
-with open('output.txt', 'w') as f:
-    f.write(str(len(output)))
