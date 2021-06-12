d1fd8f65773d33da99933b64bd298f125e388d03
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-12 21:55:16 +0900

    Create solution.py

---
+++ yandex/Lecture_2/Contest/F/solution.py
@@ -0,0 +1,15 @@
+def slow(seq):
+    for start in range(len(seq)):
+        i = start
+        j = len(seq) - 1
+        while i < len(seq) and j >= 0 and seq[i] == seq[j] and i <= j:
+            i += 1
+            j -= 1
+        if i > j:
+            ans = []
+            for i in range(start -1, -1, -1):
+                ans.append(seq[i])
+            return ans
+output = slow([1, 1, 2, 1, 1, 1]) 
+print(len(output))
+print(output)
