74c74883c0a5026e630b49946be6894a1c7f34de
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-16 23:10:26 +0900

    Update main.py

--- yandex/Lecture_5/Lecture/main.py
+++ yandex/Lecture_5/Lecture/main.py
@@ -1,1 +1,16 @@
 
+def makeprefixsum(nums):
+    prefixsum = [0] * (len(nums) + 1)
+    for i in range(1, len(nums)+1):
+        prefixsum[i] = prefixsum[i-1] + nums[i-1]
+    return prefixsum
+
+def rsq(prefixsum, l, r): # Range Sum Query
+    return prefixsum[r] - prefixsum[l]
+  
+def countzeroes(nums, l, r):
+    cnt = 0
+    for i in range(i, r):
+        if nums[i] == 0:
+            cnt += 1
+    return cnt 
