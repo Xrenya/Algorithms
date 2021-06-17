6a28300d1295b82df4c2cd727619d2fd70fd44de
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-17 12:37:22 +0900

    Update main.py

--- yandex/Lecture_5/Lecture/main.py
+++ yandex/Lecture_5/Lecture/main.py
@@ -9,7 +9,7 @@ def rsq(prefixsum, l, r): # Range Sum Query
     return prefixsum[r] - prefixsum[l]
   
 def countzeroes(nums, l, r):
-    cnt = 0
+    cnt = 0 # complexity O(NM) N - lenght of array, M - number of requests
     for i in range(i, r):
         if nums[i] == 0:
             cnt += 1
