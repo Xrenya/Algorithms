453a385123af741868f0d9f8e841194861ad4838
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-22 22:59:12 +0900

    Update main.py

--- yandex/Lecture_5/Lecture/main.py
+++ yandex/Lecture_5/Lecture/main.py
@@ -77,3 +77,16 @@ def cntpairswithdiffgtk(sortednums, k):
             last += 1
         contpairs += len(sortednums) - last
     return contpairs
+
+def bestteamsum(players):
+    bestsum = 0
+    nowsum = 0
+    last = 0
+    for first in range(len(players)):
+        while last < len(players) and (last == first or 
+                                       players[first] + players[first+1] >= players[last]):
+            nowsum += players[last]
+            last += 1
+        bestsum = max(bestsum, nowsum)
+        nowsum -= players[first]
+    return bestsum
