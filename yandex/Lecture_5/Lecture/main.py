c8e018ba67feb51d26420ba7034e8ed622c5c6cf
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-22 21:53:42 +0900

    Update main.py

--- yandex/Lecture_5/Lecture/main.py
+++ yandex/Lecture_5/Lecture/main.py
@@ -39,3 +39,21 @@ def countzerossumranges(nums):
             if rangesum == 0:
                 cntranges += 1
     return cntranges
+
+# O(n)
+def countprefixsum(nums):
+    prefixsumbyvalue = {0: 1}
+    nowsum = 0
+    for now in nums:
+        nowsum += now
+        if nowsum not in prefixsumbyvalue:
+            prefixsumbyvalue[nowsum] = 0
+        prefixsumbyvalue[nowsum] += 1
+    return prefixsumbyvalue
+
+def countzerossumrange(prefixsumbyvalue):
+    cntranges = 0
+    for nowsum in prefixsumbyvalue:
+        cntsum = prefixsumbyvalue[nowsum]
+        cntranges += cntsum * (cntsum - 1) // 2
+    return cntranges
