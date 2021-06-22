669cb701b66202d8f4a31c6d622f557b3fd1a21a
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-22 23:37:29 +0900

    Update main.py

--- yandex/Lecture_5/Lecture/main.py
+++ yandex/Lecture_5/Lecture/main.py
@@ -78,15 +78,19 @@ def cntpairswithdiffgtk(sortednums, k):
         contpairs += len(sortednums) - last
     return contpairs
 
-def bestteamsum(players):
-    bestsum = 0
-    nowsum = 0
-    last = 0
-    for first in range(len(players)):
-        while last < len(players) and (last == first or 
-                                       players[first] + players[first+1] >= players[last]):
-            nowsum += players[last]
-            last += 1
-        bestsum = max(bestsum, nowsum)
-        nowsum -= players[first]
-    return bestsum
+def merge(nums1, nums2):
+    merged = [0] * len(nums1) + [0] * len(nums2)
+    first1 = first2 = 0
+    inf = max(nums1[-1], nums2[-1]) + 1
+    nums1.append(inf)
+    nums2.append(inf)
+    for k in range(len(nums1) + len(nums2) - 2):
+        if nums1[first1] <= nums2[first2]:
+            merged[k] = nums1[first1]
+            first1 += 1
+        else:
+            merged[k] = nums2[first2]
+            first2 += 1
+    nums1.pop()
+    nums2.pop()
+    return merged
