6a54dc6ee0f6472c789a3fbfbe351116f06e4277
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-17 21:51:07 +0900

    Update main.py

--- yandex/Lecture_7/Lecture/main.py
+++ yandex/Lecture_7/Lecture/main.py
@@ -1,1 +1,15 @@
-
+def maxvisitorsonline(n, tin, tou):
+	events = []
+	for i in range(n):
+		events.append((tin[i], -1))
+		events.append((tout[i], 1))
+	events.sort()
+	online = 0
+	maxonline = 0
+	for event in events:
+		if events[1] == -1:
+			online += 1
+		else:
+			online -= 1
+		maxonline = max(online, maxonline)
+	return maxonline
