2e6428affd9703bfea168fc9afe1381f5555bb96
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-17 22:40:13 +0900

    Update main.py

--- yandex/Lecture_7/Lecture/main.py
+++ yandex/Lecture_7/Lecture/main.py
@@ -51,3 +51,19 @@ def bosscounter(n, tin, tou, m, tboss):
 			bossans.append(online)
 	return bossans
 
+def isparkingfull(cars, n):
+	events = []
+	for car in cars:
+		timein, timeout, placefrom, placeto = car
+		events.append((timein, 1, placeto - placefrom + 1))
+		events.append((timeout, -1, placeto - placefrom + 1))
+	events.sort()
+	occupied = 0
+	for i in range(len(events)):
+		if events[i][1] == -1:
+			occupied =- events[i][2]
+		elif events[i][1] == 1:
+			occupied =- events[i][2]
+		if occupied == n:
+			return True
+	return False
