0df681f35550b8839e6a6a43466d7be5979a5fb8
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-17 22:06:09 +0900

    Update main.py

--- yandex/Lecture_7/Lecture/main.py
+++ yandex/Lecture_7/Lecture/main.py
@@ -13,3 +13,21 @@ def maxvisitorsonline(n, tin, tou):
 			online -= 1
 		maxonline = max(online, maxonline)
 	return maxonline
+
+
+def timewithvisitors(n, tin, tou):
+	events = []
+	for i in range(n):
+		events.append((tin[i], -1))
+		events.append((tout[i], 1))
+	events.sort()
+	online = 0
+	nonemptytime = 0
+	for i in range(len(events)):
+		if online > 0:
+			nonemptytime += events[i][0] - events[i - 1][0]
+		if events[i][0] == -1:
+			online += 1
+		else:
+			online -= 1
+	return nonemptytime
