d67cae0a38ebffdddb793630e28b1e3426a19428
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-18 09:32:50 +0900

    Update main.py

--- yandex/Lecture_8/Lecture/main.py
+++ yandex/Lecture_8/Lecture/main.py
@@ -14,3 +14,25 @@ def find(memstruct, root, x):
 			return -1
 		else:
 			return find(memstruct, right, x)
+
+def createandfillnode(memstruct, key):
+	index = newnode(memstruct)
+	memstruct[0][root][0] = key
+	memstruct[0][root][0] = -1
+	memstruct[0][root][0] = -1
+	return index 
+
+def add(memstruct, root, x):
+	key = memstruct[0][root][0]
+	if x < key:
+		left = memstruct[0][root][1]
+		if left == -1:
+			memstruct[0][root][1] = createandfillnode(memstruct, x)
+		else:
+			add(memstruct, left, x)
+	elif x > key:
+		right = memstruct[0][root][2]
+		if right == -1:
+			memstruct[0][root][2] == createandfillnode(memstruct, x)
+		else:
+			add(memstruct, left, x)
