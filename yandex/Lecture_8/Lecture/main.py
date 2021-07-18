07426f580cdc9c0bf0b057e5d272be2b06001710
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-07-18 09:22:52 +0900

    Update main.py

--- yandex/Lecture_8/Lecture/main.py
+++ yandex/Lecture_8/Lecture/main.py
@@ -1,1 +1,16 @@
-
+def find(memstruct, root, x):
+	key = memstruct[0][root][0]
+	if k == key:
+		return root
+	elif x < key:
+		left = memstruct[0][root][1]
+		if left == -1:
+			return -1
+		else:
+			return find(memstruct, left, x)
+	elif x > key:
+		right = memstruct[0][root][2]
+		if right == -1:
+			return -1
+		else:
+			return find(memstruct, right, x)
