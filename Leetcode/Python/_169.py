67cadd18665533d925c90fa117777ad98f73abf6
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-08-02 23:22:57 +0900

    Update _169.py

--- Leetcode/Python/_169.py
+++ Leetcode/Python/_169.py
@@ -11,3 +11,15 @@ class Solution:
                 high = hashMap[num]
                 output = num
         return output
+        
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashMap = defaultdict(int)
+        high = 0
+        output = 0
+        for num in nums:
+            hashMap[num] += 1
+            if high < hashMap[num]:
+                high = hashMap[num]
+                output = num
+        return output
