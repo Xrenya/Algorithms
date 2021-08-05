9fe93552e4a9fad1169371b2bde1d16b856eaeb2
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-08-05 13:04:07 +0900

    Update main.py

--- yandex/Lecture_8/Lecture/main.py
+++ yandex/Lecture_8/Lecture/main.py
@@ -1,7 +1,7 @@
 def initmemory(maxn):
     memory = []
     for i in range(maxn):
-        memory.append([0, i + 1, 0])
+        memory.append([0, i+1, 0])
     return [memory, 0]
 
 def newnode(memstruct):
@@ -13,23 +13,23 @@ def delnode(memstruct, index):
     memory, firstfree = memstruct
     memory[index][1] = firstfree
     memstruct[1] = index
-
+    
 def find(memstruct, root, x):
-	key = memstruct[0][root][0]
-	if x == key:
-		return root
-	elif x < key:
-		left = memstruct[0][root][1]
-		if left == -1:
-			return -1
-		else:
-			return find(memstruct, left, x)
-	elif x > key:
-		right = memstruct[0][root][2]
-		if right == -1:
-			return -1
-		else:
-			return find(memstruct, right, x)
+    key = memstruct[0][root][0]
+    if x == key:
+        return root
+    elif x < key:
+        left = memstruct[0][root][1]
+        if left == -1:
+            return -1
+        else:
+            return find(memstruct, left, x)
+    elif x > key:
+        right = memstruct[0][root][2]
+        if right == - 1:
+            return -1
+        else:
+            return find(memstruct, right, x)
 
 def createandfillnode(memstruct, key):
     index = newnode(memstruct)
@@ -39,21 +39,20 @@ def createandfillnode(memstruct, key):
     return index
 
 def add(memstruct, root, x):
-  key = memstruct[0][root][0]
-	if x < key:
-		left = memstruct[0][root][1]
-		if left == -1:
-			memstruct[0][root][1] = createandfillnode(memstruct, x)
-		else:
-			add(memstruct, left, x)
-	elif x > key:
-		right = memstruct[0][root][2]
-		if right == -1:
-			memstruct[0][root][2] = createandfillnode(memstruct, x)
-		else:
-			add(memstruct, right, x)
-
-			
+    key = memstruct[0][root][0]
+    if x < key:
+        left = memstruct[0][root][1]
+        if left == -1:
+            memstruct[0][root][1] = createandfillnode(memstruct, x)
+        else:
+            add(memstruct, left, x)
+    elif x > key:
+        right = memstruct[0][root][2]
+        if right == -1:
+            memstruct[0][root][2] = createandfillnode(memstruct, x)
+        else:
+            add(memstruct, right, x)
+    
 memstruct = initmemory(20)
 root = createandfillnode(memstruct, 8)
 add(memstruct, root, 10)
