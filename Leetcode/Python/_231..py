33eef4292f47b08c1a40293dffe45b9d0002167d
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-08-26 16:28:23 +0900

    Create _231..py

---
+++ Leetcode/Python/_231..py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        power = 0
+        num = 2
+        if n == 1:
+            return True
+        elif n == 2:
+            return True
+        while num < n:
+            num *= 2
+            if num == n:
+                return True
+        return False
+      
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        cnt = 0
+        while n:
+            if n & 0x1:
+                cnt += 1
+            if cnt > 1:
+                return False
+            n >>= 1
+        if cnt == 1:
+            return True
