6df5386b370283c6e70510968b288db0e81f0260
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-11-18 15:43:06 +0900

    Update _35.py

--- Leetcode/Python/_35.py
+++ Leetcode/Python/_35.py
@@ -1,5 +1,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        l = -1
+        r = len(nums)
+        while l < r - 1:
+            mid = (r + l) // 2
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid] > target:
+                    r = mid 
+                else:
+                    l = mid 
+        return r
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         
         while l < r:
