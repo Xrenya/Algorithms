f1b34ec620bb1a1d7923f8a89bcb207fdca5b662
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-11-18 14:06:42 +0900

    Create _278.py

---
+++ Leetcode/Python/_278.py
@@ -0,0 +1,24 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            if isBadVersion(n):
+                return 1
+            
+        l = 0
+        r = n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
