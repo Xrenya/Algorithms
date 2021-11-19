3fae274aefa40de37e5270e8ee4ee987bead21b8
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-11-19 11:47:00 +0900

    Update 167.cpp

--- Leetcode/Python/167.cpp
+++ Leetcode/Python/167.cpp
@@ -16,3 +16,26 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        vector<int> result;
+        while (l < r){
+            if (numbers[l] + numbers[r] == target){
+                result.push_back(l + 1);
+                result.push_back(r + 1);
+                break;
+            }
+            else if (numbers[l] + numbers[r] > target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return result;
+    }
+};
