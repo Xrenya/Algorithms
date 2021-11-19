3661e9867bb64c8d33bf6de9082fa3e82fc5e003
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-11-19 11:00:32 +0900

    Update _167.py

--- Leetcode/Python/_167.py
+++ Leetcode/Python/_167.py
@@ -9,3 +9,11 @@ class Solution:
             else:
                 l += 1
                 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(numbers):
+            if num not in hashmap:
+                hashmap[target - num] = i
+            else:
+                return hashmap[num] + 1, i + 1
