6a794305d0628ef80054dfbf9e2c9e800bf4e1bc
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-11-22 12:26:27 +0900

    Create 414.cpp

---
+++ Leetcode/cpp/414.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long third = LONG_MIN;
+        long second = LONG_MIN;
+        long first = LONG_MIN;
+        for (int i = 0; i < nums.size(); i++){
+            if (first < nums[i]){
+                third = second;
+                second = first;
+                first = nums[i];
+            }
+            else if (second < nums[i] && nums[i] != first){
+                third = second;
+                second = nums[i];
+            }
+            else if (third < nums[i] && nums[i] != first && nums[i] != second){
+                third = nums[i];
+            }
+        }
+        return third==LONG_MIN?first:third; 
+    }
+};
