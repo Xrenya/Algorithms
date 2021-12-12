ba00d16fa54ab92b067df014725bca0ad994d206
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 17:30:45 +0900

    Update _338.py

--- Leetcode/Python/_338.py
+++ Leetcode/Python/_338.py
@@ -1,5 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        output = [0] * (n + 1)
+        for i in range(n + 1):
+            output[i] =  output[i >> 1] + i % 2
+        return output
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
         array = []
         for i in range(n + 1):
             array.append(bin(i).count("1"))
