e47155060eb2c1ac83df99b4acc97daee74ded27
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 21:18:28 +0900

    Create _920.py

---
+++ Leetcode/Python/_920.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+    def canAttendMeetings(self, intervals):
+        # Write your code here
+        end = None
+        for p in sorted(intervals, key=lambda x: x.start):
+            if end:
+                if end > p.start:
+                    return False
+                end = p.end
+            else:
+                end = p.end
+        return True
