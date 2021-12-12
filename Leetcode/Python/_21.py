56f77820abd7540d3e0cbcc9a6db74809a46ce5f
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 21:02:15 +0900

    Update _21.py

--- Leetcode/Python/_21.py
+++ Leetcode/Python/_21.py
@@ -4,6 +4,39 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(0)
+        result = head 
+        
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1 is None and list2 is None:
+            return []
+        
+        while list1 and list2:
+            if list1.val > list2.val:
+                head.next = ListNode(list2.val)
+                list2 = list2.next
+                head = head.next 
+            else:
+                head.next = ListNode(list1.val)
+                list1 = list1.next
+                head = head.next 
+                
+        while list2:
+            head.next = ListNode(list2.val)
+            list2 = list2.next
+            head = head.next 
+        while list1:
+            head.next = ListNode(list1.val)
+            list1 = list1.next
+            head = head.next 
+            
+        return result.next
+    
+class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if l1 is None and l2 is None:
             return None
