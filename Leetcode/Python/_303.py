a3383f50d05fca97c288bf8ecac25e26a075c87d
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 17:22:55 +0900

    Update _303.py

--- Leetcode/Python/_303.py
+++ Leetcode/Python/_303.py
@@ -1,6 +1,21 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        self.array = self.prefix(nums)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.array[right + 1] - self.array[left]
+        
+    def prefix(self, nums):
+        prefix = [0] * (len(nums) + 1)
+        for i in range(1, len(prefix)):
+            prefix[i] = prefix[i - 1] + nums[i - 1]
+        return prefix
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
         self.array = nums
         self.prefixsum = self.get_prefix()
 
