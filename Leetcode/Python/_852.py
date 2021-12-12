9befcbee2e35a2f874deabadde904f8ab8a947be
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 21:58:11 +0900

    Update _852.py

--- Leetcode/Python/_852.py
+++ Leetcode/Python/_852.py
@@ -1,3 +1,15 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l = 0
+        r = len(letters) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if  letters[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return letters[l % len(letters)]
+    
 class Solution(object):
     def peakIndexInMountainArray(self, A):
         """
