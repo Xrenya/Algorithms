53e7b6f3a1f3b184eec168cb6164d0548640dff9
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 22:30:30 +0900

    Update _637.py

--- Leetcode/Python/_637.py
+++ Leetcode/Python/_637.py
@@ -6,6 +6,32 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        level_map = {}
+        output = []
+
+        def findmax(root, level):
+            if level not in level_map:
+                level_map[level] = [root.val]
+            else:
+                level_map[level].append(root.val)
+
+            level += 1
+            if root.left:
+                findmax(root.left, level)
+            if root.right:
+                findmax(root.right, level)
+            if root.left == None and root.right == None:
+                return root
+
+
+        findmax(root,1)   
+
+        for key, values in level_map.items():
+            output.append(sum(values) / len(values))
+        return output
+    
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         q = deque([root])
         result = []
         
