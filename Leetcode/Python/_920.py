67597ec41caf6ed7c43f2970a96c57016d45a68c
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-12 21:22:22 +0900

    Update _920.py

--- Leetcode/Python/_920.py
+++ Leetcode/Python/_920.py
@@ -13,6 +13,19 @@ class Solution:
     """
     def canAttendMeetings(self, intervals):
         # Write your code here
+        sorted_intervals = sorted(intervals, key=lambda x: x.start)
+        for i in range(1, len(sorted_intervals)):
+            if sorted_intervals[i - 1].end > sorted_intervals[i].start:
+                return False
+        return True
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+    def canAttendMeetings(self, intervals):
+        # Write your code here
         end = None
         for p in sorted(intervals, key=lambda x: x.start):
             if end:
