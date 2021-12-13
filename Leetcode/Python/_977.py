b811b2101fa6183a2dd234627e0814bc4ab30ed6
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-13 10:10:08 +0900

    Update _977.py

--- Leetcode/Python/_977.py
+++ Leetcode/Python/_977.py
@@ -1,5 +1,23 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        array = [0] * n
+        l = 0
+        r = n - 1
+        for i in range(n-1, -1, -1):
+            left = nums[l]**2
+            right = nums[r]**2
+            if left < right:
+                array[i] = right
+                r -= 1
+            else:
+                array[i] = left
+                l += 1
+        return array
+                
+            
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
         arrayQueue = collections.deque()
         size = len(nums)
         l = 0
