6ac80739da61a0b3ee07f7412d96d28524f10c24
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-13 10:49:07 +0900

    Update _169.py

--- Leetcode/Python/_169.py
+++ Leetcode/Python/_169.py
@@ -1,3 +1,34 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        vote = 0
+        result = None
+        n = len(nums) // 2
+        for num in nums:
+            if vote == 0:
+                result = num
+            if result == num:
+                vote += 1
+                if vote > n:
+                    return result
+            else:
+                vote -= 1
+        return result
+    
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        vote = 0
+        result = None
+        
+        for num in nums:
+            if vote == 0:
+                result = num
+            if result == num:
+                vote += 1
+            else:
+                vote -= 1
+        return result
+    
+    
 from collections import defaultdict 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
