9ea97a6aada95148d7e33aa934ace1492484e104
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-20 16:48:22 +0900

    Update _2.py

--- Leetcode/Python/_2.py
+++ Leetcode/Python/_2.py
@@ -1,3 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        output = ListNode()
+        runner = output
+        reminder = 0
+        while l1 or l2:
+            cur_sum = reminder
+            
+            if l1:
+                cur_sum += l1.val
+                l1 = l1.next
+            
+            if l2:
+                cur_sum += l2.val
+                l2 = l2.next
+            
+            runner.next = ListNode(cur_sum % 10)
+            runner = runner.next
+            reminder = cur_sum // 10
+        
+        if reminder > 0:
+            runner.next = ListNode(reminder)
+        
+        return output.next
+        
+       
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
