bf215b6a2a9b527d917c8e88f0cf675f09d85863
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-12-21 17:41:33 +0900

    Update _231.py

--- Leetcode/Python/_231.py
+++ Leetcode/Python/_231.py
@@ -1,3 +1,13 @@
+import math
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        base = math.floor(math.log(n, 2))
+        return 2**base == n
+    
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         power = 0
