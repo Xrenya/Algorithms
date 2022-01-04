a7173245db4bfdc990790c53c5d07360ecffddc5
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-01-04 11:58:09 +0900

    Update _228.py

--- Leetcode/Python/_228.py
+++ Leetcode/Python/_228.py
@@ -8,19 +8,14 @@ class Solution:
         if len(nums) == 0:
             return []
         first = nums[0]
-        temp = nums[0]
         last = nums[0]
         output = []
         for num in nums[1:]:
-            if num == first:
-                continue
-            elif num - temp == 1:
+            if num - last == 1:
                 last = num
-                temp = num
             else:
                 output.append(repr(first, last))
                 first = num
                 last = num
-                temp = num
         output.append(repr(first, last))
         return output
