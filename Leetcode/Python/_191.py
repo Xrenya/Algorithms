f355252e807bbfea938f3234f574bfa4f634688b
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-01-04 11:35:23 +0900

    Create _191.py

---
+++ Leetcode/Python/_191.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        for i in range(31, -1, -1):
+            if n >> i & 1:
+                count += 1
+        return count
