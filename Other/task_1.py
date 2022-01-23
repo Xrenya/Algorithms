abf46552c66bd0e2bf865406d563bd9c127b1b8f
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-01-23 21:24:15 +0900

    Create task_1.py

---
+++ Other/task_1.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+def solution(data, n):
+    if n == 0:
+        return []
+
+    task_hash = Counter(data)
+    output = []
+    for num in data:
+        if task_hash[num] <= n:
+            output.append(num)
+    print(output)
+    return output
+
+solution([1,2, 3, 3, 2, 2, 2, 1], 2)
