5637597c3948f76635762170b1e60cbc37bd1c40
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-02-07 14:15:53 +0900

    Update _389.py

--- Leetcode/Python/_389.py
+++ Leetcode/Python/_389.py
@@ -12,3 +12,24 @@ class Solution:
         for key, val in t_counter.items():
             if val - s_counter.get(key, 0) == 1:
                 return key
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        def to_hash(s):
+            hash_dict = {}
+            for c in s:
+                if c not in hash_dict:
+                    hash_dict[c] = 0
+                hash_dict[c] += 1
+            return hash_dict
+        
+        s_dict = to_hash(s)
+        for c in t:
+            if c not in s_dict:
+                return c
+            s_dict[c] -= 1
+            if s_dict[c] == 0:
+                s_dict.pop(c)
+        return s_dict.key()
+                
+            
