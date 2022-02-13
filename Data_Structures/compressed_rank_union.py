9ccf11e810c64dffdded17b707911cf2da35c0ea
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-02-13 17:40:21 +0900

    Update compressed_rank_union.py

--- Data_Structures/compressed_rank_union.py
+++ Data_Structures/compressed_rank_union.py
@@ -10,8 +10,8 @@ class UnionFind:
         return self.root[x]
 
     def union(self, x, y):
-        root_x = self.root[x]
-        root_y = self.root[y]
+        root_x = self.find(x)
+        root_y = self.find(y)
         if root_x != root_y:
             if self.rank[root_x] > self.rank[root_y]:
                 self.root[root_y] = root_x
