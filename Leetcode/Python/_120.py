06e5cfdf65c5a26f2816c12834219e62a803bf0d
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-06-13 19:19:49 +0900

    Update _120.py

--- Leetcode/Python/_120.py
+++ Leetcode/Python/_120.py
@@ -11,3 +11,11 @@ class Solution:
         
         return min(triangle[-1])
                     
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = min(triangle[i + 1][j] + triangle[i][j], triangle[i + 1][j + 1] + triangle[i][j])
+        
+        return triangle[0][0]
+                    
