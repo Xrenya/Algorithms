b636e313dd60e0f50755e76814ee86775a0a7985
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-07-08 16:26:18 +0900

    Update and rename merge.py to merge_sort.py

--- АиСД/s01e01/merge_sort.py
+++ АиСД/s01e01/merge_sort.py
@@ -1,9 +1,11 @@
-a = [1,3,5]
-b = [2,4,6]
+
+
+a = [1,3,5,0,10,-1]
 
 def merge(a, b):
     i = 0
     j = 0
+    c = [0] * (len(a) + len(b))
     while i < len(a) or j < len(b):
         if j == len(b) or (i < len(a) and a[i] < b[j]):
             c[i + j] =  a[i]
@@ -12,5 +14,14 @@ def merge(a, b):
             c[i + j] = b[j]
             j += 1
     return c
-c = [0, 0, 0, 0, 0, 0]
-print(merge(a, b))
+    
+def sort(a):
+    n = len(a)
+    if n <= 1:
+        return a
+    pivot = len(a) // 2
+    l = sort(a[:pivot])
+    r = sort(a[pivot:])
+    return merge(l, r)
+
+print(sort(a))
