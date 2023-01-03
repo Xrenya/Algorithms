757db22f8a8782c0f49f79649ca08282563f0add
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-01-03 14:48:28 +0900

    Update _944.py

--- Leetcode/Python/_944.py
+++ Leetcode/Python/_944.py
@@ -1,3 +1,16 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        counter = 0
+        rows = len(strs)
+        cols = len(strs[0])
+        for col in range(cols):
+            for row in range(1, rows):
+                if strs[row][col] < strs[row - 1][col]:
+                    counter += 1
+                    break
+        return counter
+
+    
 from copy import copy 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
