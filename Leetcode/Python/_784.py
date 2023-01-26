23d48485cca7cc7f64a577e24c003f869874b393
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-01-26 13:49:59 +0900

    Update _784.py

--- Leetcode/Python/_784.py
+++ Leetcode/Python/_784.py
@@ -1,5 +1,23 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
+        output = [[]]
+
+        for char in s:
+            n = len(output)
+            if char.isalpha():
+                for i in range(n):
+                    output.append(output[i][:])
+                    output[i].append(char.lower())
+                    output[n + i].append(char.upper())
+            else:
+                for i in range(n):
+                    output[i].append(char)
+
+        return map("".join, output)
+
+    
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
         self.result = []
         self.travers("", s, 0)
         return self.result
