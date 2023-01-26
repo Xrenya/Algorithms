3df3e0e193630f028b6209b0026ac42980683fce
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-01-26 11:32:08 +0900

    Update _21.py

--- Leetcode/Python/_21.py
+++ Leetcode/Python/_21.py
@@ -5,6 +5,29 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list2 is None:
+            return list1
+        elif list1 is None:
+            return list2
+
+        if list1.val > list2.val:
+            head = list2
+            list2 = list2.next
+        else:
+            head = list1
+            list1 = list1.next
+
+        head.next = self.mergeTwoLists(list1, list2)
+
+        return head
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode(0)
         result = head 
         
