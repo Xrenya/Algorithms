79e793aaa9f768d0e7bf14cee75dd0628adb1c31
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-02-06 13:45:13 +0900

    Update _1470.py

--- Leetcode/Python/_1470.py
+++ Leetcode/Python/_1470.py
@@ -1,9 +1,17 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        output = [0] * len(nums)
+        for i in range(n):
+            output[i * 2] = nums[i]
+            output[i * 2 + 1] = nums[i + n]
+        return output
+    
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
         # n is middle point of the given array
         # nums - given array
         # Time complexity O(n)
-        # Space compexity O(1)
+        # Space compexity O(n)
         arr = []
         for i in range(n):
             arr.append(nums[i])
