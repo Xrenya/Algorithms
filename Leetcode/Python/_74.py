93e0ca16b4936c97bdec78a8ae39111a0bc485b7
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-02-06 13:05:43 +0900

    Update _74.py

--- Leetcode/Python/_74.py
+++ Leetcode/Python/_74.py
@@ -1,4 +1,22 @@
 class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        
+        # binary search
+        l = -1
+        r = m * n
+        while l < r - 1:
+            mid = l + (r - l) // 2
+            if matrix[mid // n][mid % n] <= target:
+                l = mid
+            else:
+                r = mid
+        return True if matrix[l // n][l % n] == target else False
+    
+class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:       
         l = -1
         row = len(matrix)
