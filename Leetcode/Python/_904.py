c3e27a609758545458e43b34e7ce0cfef2d49794
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-02-07 11:25:52 +0900

    Update _904.py

--- Leetcode/Python/_904.py
+++ Leetcode/Python/_904.py
@@ -1,5 +1,27 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        # Maximum number of fruits we can pick
+        max_picked = 0
+        left = 0
+        n = len(fruits)
+        basket = {}
+        for right in range(n):
+            add_fruit = fruits[right]
+            if add_fruit not in basket:
+                basket[add_fruit] = 0
+            basket[add_fruit] += 1
+            while len(basket) == 3:
+                remove_fruit = fruits[left]
+                basket[remove_fruit] -= 1
+                if basket[remove_fruit] == 0:
+                    del basket[remove_fruit]
+                left += 1
+            max_picked = max(max_picked, sum(basket.values()))
+
+        return max_picked
+    
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
         s = fruits
         k = 2
         counter = dict()
