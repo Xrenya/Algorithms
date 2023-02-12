8089732601e1326551d153676c80de8629b33b2c
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-02-12 22:59:12 +0900

    Create _2477.py

---
+++ Leetcode/Python/_2477.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        n = len(roads) + 1
+        adj = [[] for _ in range(n)]
+        degree = [0] * n
+
+        for road in roads:
+            adj[road[0]].append(road[1])
+            adj[road[1]].append(road[0])
+            degree[road[0]] += 1
+            degree[road[1]] += 1
+
+        return self.bfs(n, adj, degree, seats)
+
+    def bfs(self, n, adj, degree, seats):
+        q = deque()
+        for i in range(1, n):
+            if degree[i] == 1:
+                q.append(i)
+
+        representatives = [1] * n
+        fuel = 0
+
+        while q:
+            node = q.popleft()
+            fuel += ceil(representatives[node] / seats)
+            for neighbor in adj[node]:
+                degree[neighbor] -= 1
+                representatives[neighbor] += representatives[node]
+                if degree[neighbor] == 1 and neighbor != 0:
+                    q.append(neighbor)
+
+        return fuel
