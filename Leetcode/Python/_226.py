c3523c6d3c6e111bf33a1f695e1ee6b5ec242b97
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-02-18 12:10:13 +0900

    Update _226.py

--- Leetcode/Python/_226.py
+++ Leetcode/Python/_226.py
@@ -8,6 +8,20 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
+
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+
+        root.left = right
+        root.right = left
+
+        return root
+    
+    
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
         
         root.left , root.right = self.invertTree(root.right), self.invertTree(root.left)
         
