87450223d4aa01e7b6f4d11ddfe6a08b97f02a45
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-03-08 09:58:10 +0900

    Update _875.py

--- Leetcode/Python/_875.py
+++ Leetcode/Python/_875.py
@@ -1,5 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 0
+        right = max(piles) + 1
+        while left < right - 1:
+            speed = left + (right - left) // 2
+            hours_spend = 0
+            for pile in piles:
+                hours_spend += math.ceil(pile / speed)
+
+            if hours_spend <= h:
+                right = speed
+            else:
+                left = speed
+
+        return right
+    
+    
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
         def eating_time(piles, m):
             time = 0
             for pile in piles:
