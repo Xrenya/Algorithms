e56f4e29660c14d51c13cf0b869d7623a8a030de
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-03-12 12:40:44 +0900

    Update _23.py

--- Leetcode/Python/_23.py
+++ Leetcode/Python/_23.py
@@ -3,6 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class Solution:
+    def mergeList(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+
+        if l1.val < l2.val:
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+
+        head.next = self.mergeList(l1, l2)
+        return head
+
+
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        step = 1
+        n = len(lists)
+        while step < n:
+            for i in range(0, n - step, step * 2):
+                lists[i] = self.mergeList(lists[i], lists[i + step])
+            step *= 2
+        
+        return lists[0] if n else None
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 # https://stackoverflow.com/questions/40205223/priority-queue-with-tuples-and-dicts
 from queue import PriorityQueue
 
