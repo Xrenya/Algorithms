19032bde77ae72965a86064ebe22f6546d59e985
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-03-19 18:41:57 +0900

    Update _211.py

--- Leetcode/Python/_211.py
+++ Leetcode/Python/_211.py
@@ -1,6 +1,45 @@
 class WordDictionary:
 
     def __init__(self):
+        self.trie = {}
+
+    def addWord(self, word: str) -> None:
+        trie = self.trie
+        for char in word:
+            if char not in trie:
+                trie[char] = {}
+            trie = trie[char]
+        trie["$"] = True
+        
+        
+
+    def search(self, word: str) -> bool:
+        def recursive_call(word, node):
+            for i, char in enumerate(word):
+                if not char in node:
+                    if char == ".":
+                        for x in node:
+                            if x != "$" and recursive_call(word[i + 1:], node[x]):
+                                return True
+
+                    return False
+                else:
+                    node = node[char]
+
+            return "$" in node
+        return recursive_call(word, self.trie)
+
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+class WordDictionary:
+
+    def __init__(self):
         self.vocab = dict()
 
     def addWord(self, word: str) -> None:
