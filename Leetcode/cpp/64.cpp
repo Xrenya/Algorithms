fdd2ded4b207cffa2d23756e83f02336257317b1
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-03-27 10:39:19 +0900

    Update 64.cpp

--- Leetcode/cpp/64.cpp
+++ Leetcode/cpp/64.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    // 1-D solution
+    int minPathSum(vector<vector<int>>& grid) {
+      int COLS = grid[0].size(), ROWS = grid.size();
+      vector<int> mat(COLS, 0);
+      mat[0] = grid[0][0];
+      // Fill the first row
+      for (int col = 1; col < COLS; col++) {
+        mat[col] = grid[0][col] + mat[col - 1];
+      }
+      for (int row = 1; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+          if (col == 0) {
+            mat[0] += grid[row][col];
+          } else {
+            mat[col] = min(
+              grid[row][col] + mat[col - 1], grid[row][col] + mat[col]
+            );
+          }
+        } 
+      }
+
+      // for (int col = 0; col < COLS; col++) {
+      //   std::cout << mat[col] << " ";
+      // }
+      return mat[COLS - 1];
+    }
+};
+
+class Solution {
+public:
+    // 2-D solution
     int minPathSum(vector<vector<int>>& grid) {
       int COLS = grid[0].size(), ROWS = grid.size();
       vector<vector<int>> mat(ROWS, vector<int>(COLS, -1));
