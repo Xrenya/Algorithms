a6db15f0211df5d50bd25ec9e9023bcc2f11b975
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-04-21 13:18:47 +0900

    Update quick_sort.py

--- Python/Sorts/quick_sort.py
+++ Python/Sorts/quick_sort.py
@@ -1,6 +1,35 @@
 import random
 
 
+def partition(arr, left, right):
+    random_pivot = random.randint(left, right)
+    arr[left], arr[random_pivot] = arr[random_pivot], arr[left]
+
+    pivot_index = left
+
+    i = left + 1
+    for j in range(pivot_index + 1, right + 1):
+        if arr[j] <= arr[pivot_index]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[pivot_index], arr[i - 1] = arr[i - 1], arr[pivot_index]
+    pivot_index = i - 1
+    return pivot_index
+
+
+def quicksort_inplace(arr, left, right):
+    """Randomized quicksort in-place instead of copying data into new array
+    >>> array = [5,4,3,2,1]
+    >>> quicksort(array, 0, len(array) - 1)
+    [1, 2, 3, 4, 5]
+    """
+    if left < right :
+        pivot_index = partition(arr, left, right)
+        quicksort_inplace(arr, left, pivot_index - 1)
+        quicksort_inplace(arr, pivot_index + 1, right)
+        
+        
 def randomized_quicksort(arr):
     """
     This function takes an array as input and returns a sorted array using quicksort algorithm
