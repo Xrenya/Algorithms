9698f7ee31c06c176d8c796a1aa8f8b4a6bc0d47
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-04-21 10:38:38 +0900

    Update quick_sort.py
    randomized quicksort

--- Python/Sorts/quick_sort.py
+++ Python/Sorts/quick_sort.py
@@ -1,3 +1,30 @@
+import random
+
+
+def randomized_quicksort(arr):
+    """
+    This function takes an array as input and returns a sorted array using quicksort algorithm
+    """
+    # base case: if the array has less than two elements, it is already sorted
+    if len(arr) < 2:
+        return arr
+
+    # choose the random pivot element's index
+    pivot_index = random.randint(0, len(arr) - 1)
+    pivot = arr[pivot_index]
+
+    # partition the array into two subarrays based on the pivot element
+    left = [arr[idx] for idx in range(len(arr)) if arr[idx] <= pivot and idx != pivot_index]
+    right = [arr[idx] for idx in range(len(arr)) if arr[idx] > pivot and idx != pivot_index]
+
+    # recursive calls to quicksort on left and right subarrays
+    left_sorted = randomized_quicksort(left)
+    right_sorted = randomized_quicksort(right)
+
+    # concatenate the sorted subarrays with the pivot element in between
+    return left_sorted + [pivot] + right_sorted
+
+
 def quicksort(arr):
     """
     This function takes an array as input and returns a sorted array using quicksort algorithm
