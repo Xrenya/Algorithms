24e04204909d3c50be8296e40c51a58a4955694c
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-04-26 11:11:07 +0900

    Update _258.py

--- Leetcode/Python/_258.py
+++ Leetcode/Python/_258.py
@@ -16,3 +16,11 @@ class Solution:
 class Solution:
     def addDigits(self, num: int) -> int:
         return 1 + (num - 1) % 9 if num else 0
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
