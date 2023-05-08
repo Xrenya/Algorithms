3bb96c24c62cad41aae1174fe5ede7b7e832e22a
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-08 09:18:09 +0900

    Update _1572.py

--- Leetcode/Python/_1572.py
+++ Leetcode/Python/_1572.py
@@ -1,9 +1,21 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
+        rows = len(mat)
+        acc = 0
+        for row in range(rows):
+            if row != rows - row - 1:
+                acc += mat[row][row] + mat[row][rows - row - 1]
+            else:
+                acc += mat[row][row]
+        return acc
+    
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
         # Time complexity O(n)
         acc = 0
-        for idx in range(len(mat)):
-            acc += mat[idx][idx] + mat[idx][len(mat)-1-idx]
-        if len(mat) %2 == 1:
-            acc -= mat[len(mat)//2][len(mat)//2]
+        n = len(mat)
+        for idx in range(n):
+            acc += mat[idx][idx] + mat[idx][n - 1 - idx]
+        if len(mat) % 2 == 1:
+            acc -= mat[n // 2][n // 2]
         return acc
