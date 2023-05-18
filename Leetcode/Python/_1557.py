762a9f293b9379dad47e2e048866a2f65a4a49ed
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-18 09:21:19 +0900

    Update _1557.py

--- Leetcode/Python/_1557.py
+++ Leetcode/Python/_1557.py
@@ -1,10 +1,21 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        indegree=[0]*n
-        for i,j in edges:
-            indegree[j]+=1
-        lst=[]
+        output = set(list(range(n)))
+        for u, v in edges:
+            if v in output:
+                output.remove(v)
+                
+        return output
+    
+    
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        indegree = [0] * n
+        for i, j in edges:
+            indegree[j] += 1
+        lst = []
         for i in range(n):
-            if indegree[i]==0:
+            if indegree[i] == 0:
                 lst.append(i)
         return lst
+    
