7ef02975724d5d4bd97ad2b55877f9c52594881b
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-22 09:26:05 +0900

    Update _347.py

--- Leetcode/Python/_347.py
+++ Leetcode/Python/_347.py
@@ -1,5 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapping = {}
+        for num in nums:
+            if num not in mapping:
+                mapping[num] = 0
+            mapping[num] += 1
+        output = []
+        for k, val in sorted(mapping.items(), key=lambda item: item[1], reverse=True)[:k]:
+            output.append(k)
+        return output
+    
+    
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = {}
         for num in nums:
             if num not in counter:
