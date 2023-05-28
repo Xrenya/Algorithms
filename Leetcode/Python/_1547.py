78d91ba4441af024ac4b39eda6afd1d6aa283a61
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-28 20:47:51 +0900

    Create _1547.py

---
+++ Leetcode/Python/_1547.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        memo = {}
+        cuts = [0] + sorted(cuts) + [n]
+
+        def cost(left, right):
+            if (left, right) in memo:
+                return memo[(left, right)]
+
+            if right - left == 1:
+                return 0
+            
+            output = min(
+                cost(left, mid) + cost(mid, right) + cuts[right] - cuts[left] for mid in range(left + 1, right)
+            )
+            memo[(left, right)] = output
+            return output
+
+        return cost(0, len(cuts) - 1)
