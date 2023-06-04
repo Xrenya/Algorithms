75c946a39a5294f87b60d8c46f1ccd07092c2464
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-06-04 19:00:15 +0900

    Update _547.py

--- Leetcode/Python/_547.py
+++ Leetcode/Python/_547.py
@@ -27,7 +27,48 @@ class Solution:
                 if u != v and isConnected[u][v] == 1:
                     union_fold.union(u, v)
         return len(set(union_fold.list))
-      
+
+class Union(object):
+    def __init__(self, size: int):
+        self.size = size
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.root[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.root[root_x] = root_y
+            else:
+                self.root[root_y] = root_x
+                self.rank[root_x] += 1
+
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+class Solution:
+    def findCircleNum(self, isConnected):
+        n = len(isConnected)
+        union = Union(n)
+        for i in range(n):
+            for j in range(len(isConnected[0])):
+                if isConnected[i][j] == 1:
+                    union.union(i, j)
+        for i in range(len(isConnected)):
+            union.find(i)
+            
+        return len(list(set(union.root))) 
+
         
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
