2398814e3bce1603844409875fb167c061251985
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-06-19 16:30:07 +0900

    Update _1732.py

--- Leetcode/Python/_1732.py
+++ Leetcode/Python/_1732.py
@@ -1,5 +1,14 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        cur_value = 0
+        max_value = 0
+        for n in gain:
+            cur_value += n
+            max_value = max(max_value, cur_value)
+        return max_value
+    
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
         highest = 0
         temp = 0
         for num in gain:
