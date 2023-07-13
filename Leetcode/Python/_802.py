9101ef0f3cba466102df096ccb0809db94b5e7ad
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-07-13 23:10:00 +0900

    Update _802.py

--- Leetcode/Python/_802.py
+++ Leetcode/Python/_802.py
@@ -1,5 +1,41 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        indegree = [0] * n
+
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbour in adj[node]:
+                indegree[neighbour] -= 1
+                if indegree[neighbour] == 0:
+                    q.append(neighbour)
+
+            
+        safe_nodes = []
+        for i in range(n):
+            if safe[i]:
+                safe_nodes.append(i)
+
+        return safe_nodes
+
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         N = len(graph)
         safe = [False] * N
 
