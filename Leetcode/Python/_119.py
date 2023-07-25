6d7a28558e095d1b361e1dbd5149253b453a1878
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-07-25 11:16:56 +0900

    Update _119.py

--- Leetcode/Python/_119.py
+++ Leetcode/Python/_119.py
@@ -1,4 +1,27 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
+        
+    def getCol(self, row, col):
+        if row == 0 or col == 0 or row == col:
+            return 1
+
+        if (row, col) in self.memo:
+            return self.memo[(row, col)]
+
+        self.memo[(row, col)] = self.getCol(row - 1, col - 1) + self.getCol(row - 1, col)
+        
+        return self.memo[(row, col)]
+
+    def getRow(self, rowIndex: int) -> List[int]:
+        output = []
+        
+        for row in range(rowIndex + 1):
+            output.append(self.getCol(rowIndex, row))
+        return output
+
+
+class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         output = [1, 1]
         if rowIndex == 0:
