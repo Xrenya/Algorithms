8d72236d05f573daa5adba4c5e25c15c54806831
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-07-27 23:43:30 +0900

    Update _215.py

--- Leetcode/Python/_215.py
+++ Leetcode/Python/_215.py
@@ -1,3 +1,46 @@
+class HeapMax:
+    def __init__(self):
+        self.a = []
+        self.size = 0
+        
+    def insert(self, v):
+        self.a.append(v)
+        self.size += 1
+        i = self.size - 1
+        while i > 0 and self.a[i] > self.a[(i - 1) // 2]:
+            self.a[i], self.a[(i - 1) // 2] = self.a[(i - 1) // 2], self.a[i]
+            i = (i - 1) // 2
+            
+    def remove(self):
+        self.a[0], self.a[-1] = self.a[-1], self.a[0]
+        last = self.a.pop()
+        self.size -= 1
+        i = 0
+        while 2 * i + 1 < self.size:
+            j = 2 * i + 1
+            if 2 * i + 2 < self.size and self.a[j] < self.a[2 * i + 2]:
+                j = 2 * i + 2
+            if self.a[i] > self.a[j]:
+                break
+            else:
+                self.a[i], self.a[j] = self.a[j], self.a[i]
+                i = j5 
+        
+        return last
+    
+        
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = HeapMax()
+        for n in nums:
+            heap.insert(n)
+            
+        for _ in range(k):
+            v = heap.remove()
+            
+        return v
+
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:                    
         n = len(nums)
