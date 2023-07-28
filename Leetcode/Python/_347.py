62c2d840860148d316e59ff3215709aebcb49bad
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-07-28 09:54:23 +0900

    Update _347.py

--- Leetcode/Python/_347.py
+++ Leetcode/Python/_347.py
@@ -1,3 +1,46 @@
+class HeapMax:
+    def __init__(self):
+        self.a = []
+        self.size = 0
+        
+    def insert(self, v):
+        self.a.append(v)
+        self.size += 1
+        i = self.size - 1
+        while i > 0 and self.a[i][1] > self.a[(i - 1) // 2][1]:
+            self.a[i], self.a[(i - 1) // 2] = self.a[(i - 1) // 2], self.a[i]
+            i = (i - 1) // 2
+            
+    def remove(self):
+        self.a[-1], self.a[0] = self.a[0], self.a[-1]
+        last = self.a.pop()
+        self.size -= 1
+        i = 0
+        while 2 * i + 1 < self.size:
+            j = 2 * i + 1
+            if 2 * i + 2 < self.size and self.a[j][1] < self.a[2 * i + 2][1]:
+                j = 2 * i + 2
+            if self.a[i][1] > self.a[j][1]:
+                break
+            else:
+                self.a[i], self.a[j] = self.a[j], self.a[i]
+                i = j
+        return last[0]
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = HeapMax()
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += 1
+        for (k_, v_) in count.items():
+            heap.insert((k_, v_))
+        output = [0] * k
+        for i in range(k):
+            output[i] = heap.remove()
+        return output
+        
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mapping = {}
