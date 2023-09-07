43d2833c27886bf5cf961478f79cf1a6ae24747f
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-09-07 17:05:38 +0900

    Create random_choice.py

---
+++ Other/random_choice.py
@@ -0,0 +1,23 @@
+import random
+
+# probs = [0.1, 0.4, 0.2, 0.3]
+# size = 7
+# random.random() ~ U(0, 1)
+
+
+def prefix(probs):
+    prob = probs
+    for i in range(1, len(prob)):
+        prob[i] += probs[i - 1]
+    return prob
+
+def weighted_choice(probs, size):
+    p = prefix(probs)
+    output = []
+    for i in range(size):
+        proba = random.random()
+        for i in range(len(p)):
+            if proba <= p[i]:
+                output.append(i)
+                break
+    return output
