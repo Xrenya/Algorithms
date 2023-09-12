0fcc56b7030856a7b739ad34b4c045574e910d7f
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-09-12 10:41:43 +0900

    Update _1647.py

--- Leetcode/Python/_1647.py
+++ Leetcode/Python/_1647.py
@@ -1,5 +1,23 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
+        count = Counter(s)
+        deletions = 0
+        used_freq = set()
+
+        heap = list(count.values())
+        heapq.heapify(heap)
+
+        while heap:
+            freq = heapq.heappop(heap)
+            while freq > 0 and freq in used_freq:
+                freq -= 1
+                deletions += 1
+            used_freq.add(freq)
+        return deletions
+
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
         hashmap = defaultdict(int)
         myset = set()
         for c in s:
