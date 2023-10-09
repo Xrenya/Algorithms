1435fdef179c2c0590648d7135e54012aecec5c4
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-10-09 14:02:55 +0900

    Update _34.py

--- Leetcode/Python/_34.py
+++ Leetcode/Python/_34.py
@@ -1,4 +1,30 @@
 class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def binsearch(arr, target, lower=True):
+            l = -1
+            r = len(arr)
+            while l < r - 1:
+                m = l + (r - l) // 2
+                if lower:
+                    if arr[m] < target:
+                        l = m
+                    else:
+                        r = m
+                else:
+                    if arr[m] <= target:
+                        l = m
+                    else:
+                        r = m
+            return r if lower else l
+
+        left = binsearch(nums, target, True)
+        right = binsearch(nums, target, False)
+        if 0 <= left < len(nums) and left <= right and nums[left] == target:
+            return [left, right]
+        return [-1, -1]
+
+
+class Solution:
     def searchRange(self, nums, target):
         def get_boundary(nums, target, lower):
             n = len(nums)
