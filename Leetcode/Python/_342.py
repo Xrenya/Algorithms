f50eda761cac9f5d374f23d4c57dff11e6d5383a
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-10-23 10:20:14 +0900

    Update _342.py

--- Leetcode/Python/_342.py
+++ Leetcode/Python/_342.py
@@ -1,3 +1,23 @@
+class Power(object):
+    def __init__(self):
+        self.max_power = 15
+        self.nums = 1
+    
+    def __iter__(self):
+        for i in range(self.max_power + 1):
+            yield self.nums
+            self.nums *= 4
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        power = Power()
+        for num in power:
+            if n == num:
+                return True
+        return False
+
+
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n <= 0:
