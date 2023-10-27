05908def0b1405f6cb03d1f892462f6b0bd9cbf2
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-10-27 13:12:00 +0900

    Update _5.py

--- Leetcode/Python/_5.py
+++ Leetcode/Python/_5.py
@@ -1,4 +1,22 @@
 class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        max_len = 1
+        output = '' if not s else s[0]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = True
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    if j - i == 1 or dp[i + 1][j - 1]:
+                        dp[i][j] = True
+                        if max_len <= j - i + 1:
+                            max_len = j - i + 1
+                            output = s[i:j + 1]
+        return output
+
+
+class Solution:
     def longestPalindrome(self, s):
             longest_palindrom = ''
             dp = [[0]*len(s) for _ in range(len(s))]
