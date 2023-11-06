b140e3efcbd532ce151c5612be5c3d9cdd92318c
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-11-06 10:16:31 +0900

    Create _1845.py

---
+++ Leetcode/Python/_1845.py
@@ -0,0 +1,16 @@
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.seats = [i for i in range(1, n + 1)]
+
+    def reserve(self) -> int:
+        seat = heapq.heappop(self.seats)
+        return seat
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.seats, seatNumber)
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
