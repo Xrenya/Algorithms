48ba5920b89bc83a0fe93e4a7e31fcbf15848a6f
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-11-16 16:30:00 +0900

    Update _657.py

--- Leetcode/Python/_657.py
+++ Leetcode/Python/_657.py
@@ -1,5 +1,20 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for move in moves:
+            if move == "U":
+                y += 1
+            elif move == "D":
+                y -= 1
+            elif move == "L":
+                x -= 1
+            else:
+                x += 1
+        return x == 0 and y == 0
+
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
         starting_point = [0, 0]
         hashMap = {"R": [1, 0], "L": [-1, 0],
                    "U": [0, 1], "D": [0, -1]}
@@ -7,7 +22,8 @@ class Solution:
             starting_point[0] += hashMap[move][0]
             starting_point[1] += hashMap[move][1]
         return starting_point == [0, 0]
-        
+
+
 class Solution:
     def judgeCircle(self, m: str) -> bool:
         hashMap = {"R": 0, "L": 0,
@@ -17,7 +33,8 @@ class Solution:
         if hashMap["R"]==hashMap["L"] and hashMap["U"]==hashMap["D"]:
             return True
         return False
-     
+
+
 class Solution:
     def judgeCircle(self, m: str) -> bool:
         hashMap = {"R": 0, "L": 0,
