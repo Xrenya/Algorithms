7f4d31b28f8be33349fd0ac18bfb0f7adbf3340e
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-11-29 16:01:58 +0900

    Update quick_sort_Hoare_partition.py

--- АиСД/S01E03/quick_sort_Hoare_partition.py
+++ АиСД/S01E03/quick_sort_Hoare_partition.py
@@ -1,33 +1,36 @@
 import random
 
 
-def qsort(array, left, right):
+def qsort(array, low, high):
     if left < right:
-        pivot = random.randint(left, right)
-        random_index = partition(array, left, right)
-        qsort(array, left, random_index)
-        qsort(array, random_index + 1, right)
+        pivot = random.randint(low, high)
+        random_index = partition(array, low, high)
+        qsort(array, low, random_index)
+        qsort(array, random_index + 1, high)
 
 
-def partition(array, left, right):
-    pivot = array[random.randint(left, right)]
-    i = left
-    j = right
+def partition(a, low, high):
+    pivot = a[random.randint(low, high)]
+    i = low - 1
+    j = high + 1
     while True:
-        while array[i] < pivot:
+        i += 1
+        while a[i] < pivot:
             i += 1
-
-        while array[j] > pivot:
+            
+        j -= 1
+        while a[j] > pivot:
             j -= 1
+            
         if i >= j:
             return j
+        a[i], a[j] = a[j], a[i]
 
-        array[i], array[j] = array[j], array[i]
 
 
 array = [random.randint(-10, 100) for _ in range(5)]
-left = 0
-right = len(array) - 1
+low = 0
+high = len(array) - 1
 print(array)
-qsort(array, left, right)
+qsort(array, low, high)
 print(array)
