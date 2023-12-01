c3b22a3ae14427f8f64ac386b77372c30f55afee
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-12-01 11:04:13 +0900

    Update _1662.py

--- Leetcode/Python/_1662.py
+++ Leetcode/Python/_1662.py
@@ -1,7 +1,8 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         return "".join(word1) == "".join(word2)
-      
+
+
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         string1 = ""
