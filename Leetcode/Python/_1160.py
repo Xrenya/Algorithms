eaaed2087957eaea1cc3da28a8713a8fadbb58e5
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-12-02 00:43:24 -0800

    Update _1160.py

--- Leetcode/Python/_1160.py
+++ Leetcode/Python/_1160.py
@@ -1,5 +1,31 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        def to_mapping(s):
+            d = {}
+            for char in s:
+                if char not in d:
+                    d[char] = 0
+                d[char] += 1
+            return d
+
+        chars = to_mapping(chars)
+        output = 0
+        for word in words:
+            w = to_mapping(word)
+            add = True
+            for k, v in w.items():
+                if k not in chars or v > chars[k]:
+                    add = False
+                    break
+
+            if add:
+                output += len(word)
+        return output
+
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
         hashMap = {}
         for letter in chars:
             if letter not in hashMap:
@@ -22,7 +48,8 @@ class Solution:
             if string == word:
                 acc += len(word)
         return acc
-          
+
+
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         hashMap = collections.Counter(chars)
