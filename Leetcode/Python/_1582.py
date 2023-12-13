fe894343dce34148584724e8681df6a46c952b5a
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-12-13 10:33:48 +0900

    Update _1582.py

--- Leetcode/Python/_1582.py
+++ Leetcode/Python/_1582.py
@@ -1,5 +1,27 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
+        rows = len(mat)
+        cols = len(mat[0])
+        row_c = [0] * rows
+        col_c = [0] * cols
+        
+        for row in range(rows):
+            for col in range(cols):
+                if mat[row][col]:
+                    row_c[row] += 1
+                    col_c[col] += 1
+                    
+        output = 0
+        for row in range(rows):
+            for col in range(cols):
+                if mat[row][col]:
+                    if row_c[row] == 1 and col_c[col] == 1:
+                        output += 1
+        return output
+
+
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
         count = 0
         for col in zip(*mat):
             res, idx = self.summator(col)
