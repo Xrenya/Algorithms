f47db1c8f6f2971dd7205af31eda473466e7feab
Rinat <51479797+Xrenya@users.noreply.github.com>
2024-02-22 22:27:57 +0900

    Update _73.py

--- Leetcode/Python/_73.py
+++ Leetcode/Python/_73.py
@@ -3,6 +3,45 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        row_flag, col_flag = False, False
+        ROWS, COLS = len(matrix), len(matrix[0])
+        for row in range(ROWS):
+            if matrix[row][0] == 0:
+                col_flag = True
+                break
+        
+        for col in range(COLS):
+            if matrix[0][col] == 0:
+                row_flag = True
+                break
+
+        for row in range(1, ROWS):
+            for col in range(1, COLS):
+                if matrix[row][col] == 0:
+                    matrix[0][col] = 0
+                    matrix[row][0] = 0
+
+        for row in range(1, ROWS):
+            for col in range(1, COLS):
+                if matrix[0][col] == 0:
+                    matrix[row][col] = 0
+                if matrix[row][0] == 0:
+                    matrix[row][col] = 0
+
+        if col_flag:
+            for row in range(ROWS):
+                matrix[row][0] = 0
+
+        if row_flag:
+            for col in range(COLS):
+                matrix[0][col] = 0
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         row_zeros = False
         col_zeros = False
         
