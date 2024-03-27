cc30c86b83d63b136cc7407c06e0b684b20b45ed
Rinat <51479797+Xrenya@users.noreply.github.com>
2024-03-27 11:28:28 +0900

    Update _442.py

--- Leetcode/Python/_442.py
+++ Leetcode/Python/_442.py
@@ -1,5 +1,25 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        i = 0
+        # Cycle sort O(n)
+        while i < n:
+            correct_idx = nums[i] - 1
+            if nums[i] != nums[correct_idx]:
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+            else:
+                i += 1
+                
+        output = []
+        for i in range(n):
+            if nums[i] - 1 != i:
+                output.append(nums[i])
+                
+        return output
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
         result = []
         
         for num in nums:
