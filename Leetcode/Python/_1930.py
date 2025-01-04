2a1a6acf7b149a446955dbb9c9c0df0f37aa9e3c
Rinat <51479797+Xrenya@users.noreply.github.com>
2025-01-04 16:35:43 +0900

    Update _1930.py

--- Leetcode/Python/_1930.py
+++ Leetcode/Python/_1930.py
@@ -24,3 +24,21 @@ class Solution:
             output += len(between)
 
         return output
+
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        letters = set(s)
+        ans = 0
+        
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+            
+            for k in range(i + 1, j):
+                between.add(s[k])
+            
+            ans += len(between)
+
+        return ans
+        
