7b228252c04240011c30b480012f5c2728e3502b
Rinat <51479797+Xrenya@users.noreply.github.com>
2025-01-07 23:37:43 +0900

    Update _1408.py

--- Leetcode/Python/_1408.py
+++ Leetcode/Python/_1408.py
@@ -1,5 +1,17 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
+        words.sort(key=lambda x: len(x))
+        output = []
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if words[i] in words[j]:
+                    output.append(words[i])
+                    break
+        return output
+
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
         output = []
         words = sorted(words, key=lambda x: len(x), reverse=True)
         flag = True
