e46e46b99e0e7831d3ee807ff45246d1cc02c1e4
Rinat <51479797+Xrenya@users.noreply.github.com>
2025-01-28 11:23:49 -0800

    Create _2658.py

---
+++ Leetcode/Python/_2658.py
@@ -0,0 +1,35 @@
+class Solution:
+    def calc_fishes(self, grid, visited, row, col):
+        if (
+            row < 0
+            or row >= len(grid)
+            or col < 0
+            or col >= len(grid[0])
+            or grid[row][col] == 0
+            or visited[row][col]
+        ):
+            return 0
+
+        visited[row][col] = True
+        return (
+            grid[row][col]
+            + self.calc_fishes(grid, visited, row, col + 1)
+            + self.calc_fishes(grid, visited, row, col - 1)
+            + self.calc_fishes(grid, visited, row + 1, col)
+            + self.calc_fishes(grid, visited, row - 1, col)
+        )
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        max_fish_count = 0
+
+        visited = [[False] * cols for _ in range(rows)]
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] > 0 and not visited[row][col]:
+                    max_fish_count = max(
+                        max_fish_count,
+                        self.calc_fishes(grid, visited, row, col),
+                    )
+
+        return max_fish_count
