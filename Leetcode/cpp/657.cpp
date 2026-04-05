4bb8ebbbcff470362a7d9da4d4201651b837e99b
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-04-05 22:52:01 +0300

    Update 657.cpp

--- Leetcode/cpp/657.cpp
+++ Leetcode/cpp/657.cpp
@@ -1,6 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
 class Solution {
 public:
-    bool judgeCircle(string moves) {
+    bool judgeCircle(std::string moves) {
         int x = 0, y = 0;
         for (auto m : moves) {
             if (m == 'U') {
@@ -16,3 +28,13 @@ public:
         return x == 0 & y == 0;
     }
 };
+
+
+int main()
+{
+    Solution sol;
+    assert (sol.judgeCircle("UD") == true);
+    assert (sol.judgeCircle("UUD") == false);
+    std::cout << "Tests are passed!" << std::endl;
+    return 0;
+}
