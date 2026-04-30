1a4be0d83fd4583b81fcdf6ba9468ced657157e2
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-04-30 14:27:48 +0300

    Update 67.cpp

--- Leetcode/cpp/67.cpp
+++ Leetcode/cpp/67.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <string>
-
+#include <cassert>
+#include <queue>
+#include <algorithm>
 
 class Solution {
 public:
-    string addBinary(std::string a, std::string b) {
+    std::string addBinary(std::string a, std::string b) {
+        int i = static_cast<int>(a.size()) - 1;
+        int j = static_cast<int>(b.size()) - 1;
+        int carry = 0;
+        std::string output;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            if (i >= 0) {
+                carry += (a[i--] - '0');
+            }
+            if (j >= 0) {
+                carry += (b[j--] - '0');
+            }
+            output.push_back((carry % 2) ? '1' : '0');
+            carry /= 2;
+        }
+        std::reverse(output.begin(), output.end());
+        return output;
+    }
+
+    std::string addBinaryNotOptimal(std::string a, std::string b) {
         int asz = a.size() - 1;
         int bsz = b.size() - 1;
         int cur = 0;
@@ -18,3 +39,14 @@ public:
         return output;
     }
 };
+int main() {
+    Solution sol;
+    std::string a = "11";
+    std::string b = "1";
+    std::string expected_output = "100";
+    std::string output = sol.addBinary(a, b);
+    
+    assert(output == expected_output && "Test #1 failed!");
+    std::cout << "Tests are passed!\n";
+    return 0;
+}
