edde1a12cf92df8f1961a36fcaf87eeda7b2f3ff
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-01 14:20:44 +0300

    Create 759.cpp

---
+++ Leetcode/cpp/759.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    std::vector<Interval> employeeFreeTime(std::vector<std::vector<Interval>> schedule) {
+        std::vector<std::pair<int, int>> data;
+        for (auto times : schedule) {
+            for (auto time : times) {
+                data.push_back({time.start, 1});
+                data.push_back({time.end, -1});
+            }
+        }
+        std::sort(data.begin(), data.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+            if (a.first != b.first) return a.first < b.first;
+            return a.second > b.second; // При равном времени сначала +1 (начало), потом -1 (конец)
+        });
+        std::vector<Interval> output;
+        int last = -1;
+        int total = 0;
+        for (auto d : data) {
+            total += d.second;
+            if (last != -1) {
+                Interval interv;
+                interv.start = last;
+                interv.end = d.first;
+                output.push_back(interv);
+                last = -1;
+            } else if (total == 0 && last == -1) {
+                last = d.first;
+            }
+        }
+        return output;
+    }
+};
