4e9a50a01f6d74966d86095039e6174a5f5429ab
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-02 15:59:19 +0300

    Update 64.cpp

--- Leetcode/cpp/64.cpp
+++ Leetcode/cpp/64.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <string>
+#include <climits>
+
+class Solution {
+public:
+  int minPathSum(std::vector<std::vector<int>>& grid) {
+    int m = static_cast<int>(grid.size());
+    int n = static_cast<int>(grid[0].size());
+
+    for (int i = 1; i < m; ++i) {
+      grid[i][0] += grid[i - 1][0];
+    }
+    
+    for (int j = 1; j < n; ++j) {
+      grid[0][j] += grid[0][j - 1];
+    }
+
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        grid[i][j] += std::min(grid[i - 1][j], grid[i][j - 1]);
+      }
+    }
+    // debug:
+    // for (int i = 0; i < m; ++i) {
+    //   for (int j = 0; j < n; ++j) {
+    //     std::cout << grid[i][j] << " ";
+    //   }
+    //   std::cout << std::endl;
+    // }
+    return grid[m - 1][n - 1];
+  }
+};
+
+int main() {
+  Solution sol;
+  std::vector<std::vector<int>> grid = { {1, 3, 1}, {1, 5, 1}, {4, 2, 1} };
+  int expected_output = 7;
+  int output = sol.minPathSum(grid);
+  assert ((output == expected_output) && "Test #1 failed!");
+  std::cout << "Tests are passed!" << std::endl;
+  
+  return 0;
+}
+
+
+/*
 class Solution {
 public:
     // 2-D solution without extra space: O(1)
@@ -90,3 +139,4 @@ public:
       return mat[ROWS - 1][COLS - 1];
     }
 };
+*/
