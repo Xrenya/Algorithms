c0582263cd6fba8a0a1ea538d64cd066da71489e
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-15 13:28:57 +0300

    Create 153.cpp

---
+++ Leetcode/cpp/153.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+class Solution {
+public:
+    int findMin(std::vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+          int m = left + (right - left) / 2;
+          if (nums[m] > nums[n - 1]) {
+            left = m + 1;
+          } else {
+            right = m;
+          }
+        }
+        return nums[left];
+    }
+};
+
+
+int main() {
+    Solution sol;
+    std::vector<int> nums = {5, 6, 1, 2, 3, 4};
+    int exected_output = 1;
+    int output = sol.findMin(nums);
+    
+    assert((output == exected_output) && "Test #1 failed!");
+    
+    
+    nums = {6, 5, 4, 3};
+    exected_output = 3;
+    output = sol.findMin(nums);
+    assert((output == exected_output) && "Test #2 failed!");
+    
+    
+    nums = {1, 2, 3, 4, 5, 6};
+    exected_output = 1;
+    output = sol.findMin(nums);
+    assert((output == exected_output) && "Test #2 failed!");
+    
+    std::cout << "Tests are passed!" << std::endl;
+    
+    return 0;
+}
+
