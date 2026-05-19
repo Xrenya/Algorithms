b2c72ea84a8ba84749aa8647ce3e3bac96c11c5d
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-19 17:09:32 +0300

    Update 1700.cpp

--- Leetcode/cpp/1700.cpp
+++ Leetcode/cpp/1700.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <deque>
 
 class Solution {
 public:
+     int countStudentsV2(std::vector<int>& students, std::vector<int>& sandwiches) {
+        int n = static_cast<int>(students.size());
+        std::deque<int> qStudent(students.begin(), students.end());
+        std::deque<int> qSandwiches(sandwiches.begin(), sandwiches.end());
+        
+        int cur_iter = n;
+        
+        while (!qStudent.empty()) {
+            int front = qStudent.front();
+            qStudent.pop_front();
+            int type = qSandwiches.front();
+            
+            if (front == type) {
+                qSandwiches.pop_front();
+                cur_iter = static_cast<int>(qStudent.size());
+            } else {
+                qStudent.push_back(front);
+                --cur_iter;
+            }
+            
+            if (cur_iter == 0) {
+                break;
+            }
+        }
+        return static_cast<int>(qStudent.size());
+    }
     int countStudents(std::vector<int>& students, std::vector<int>& sandwiches) {
         int n = static_cast<int>(students.size());
         int count[2] = {0, 0};
@@ -30,6 +57,9 @@ int main() {
     int output = sol.countStudents(students, sandwiches);
     
     assert((expected_output == output) && "Test #1 failed!");
+    
+    output = sol.countStudentsV2(students, sandwiches);
+    assert((expected_output == output) && "Test #2 failed!");
     
     std::cout << "Tests are passed!" << std::endl;
     return 0;
