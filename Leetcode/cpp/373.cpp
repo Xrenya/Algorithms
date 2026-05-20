3fa91993a386efdfd0f7af5f66954ea5f97fa916
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-20 17:37:39 +0900

    Update 373.cpp

--- Leetcode/cpp/373.cpp
+++ Leetcode/cpp/373.cpp
@@ -38,6 +38,43 @@ public:
         }
         return output;
     }
+    std::vector<std::vector<int>> kSmallestPairsCmp(std::vector<int>& nums1, std::vector<int>& nums2, int k) {
+        int m = static_cast<int>(nums1.size());
+        int n = static_cast<int>(nums2.size());
+
+        auto cmpMinHeap = [](std::pair<int, std::pair<int, int>>a, std::pair<int, std::pair<int, int>> b) {
+            return a.first > b.first;
+        };
+        std::priority_queue<
+            std::pair<int, std::pair<int, int>>, 
+            std::vector<std::pair<int, std::pair<int, int>>>, 
+            decltype(cmpMinHeap)
+        > minHeap(cmpMinHeap);
+
+        std::set<std::pair<int, int>> visited;
+        std::vector<std::vector<int>> output;
+        minHeap.push({nums1[0] + nums2[0], {0, 0}});
+        visited.insert({0, 0});
+
+        while (k-- && !minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+            int i = top.second.first;
+            int j = top.second.second;
+
+            output.push_back({nums1[i], nums2[j]});
+
+            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
+                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
+                visited.insert({i + 1, j});
+            }
+            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
+        }
+        return output;
+    }
 };
 
 int main() {
@@ -47,6 +84,9 @@ int main() {
     std::vector<std::vector<int>> expected_output = {{1,2},{1,4},{1,6}};
     std::vector<std::vector<int>> output = sol.kSmallestPairs(nums1, nums2, k);
     assert(output == expected_output && "Test #1 failed!");
+    
+    output = sol.kSmallestPairsCmp(nums1, nums2, k);
+    assert(output == expected_output && "Test #2 failed!");
 
     std::cout << "Tests are passed!\n";
     return 0;
