1cc9301f8aa4ce543379422f7c6c35d2412d2c38
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-05-27 10:51:18 +0300

    Update 3121.cpp

--- Leetcode/cpp/3121.cpp
+++ Leetcode/cpp/3121.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <cassert>
 #include <string>
+#include <vector>
 
 class Solution {
 public:
@@ -30,6 +31,27 @@ public:
         }
         return output;
     }
+    
+    int numberOfSpecialCharsWithArray(std::string word) {
+        std::vector<int> alpha(52, -1);
+        for (int i = 0; i < static_cast<int>(word.length()); ++i) {
+            char ch = word[i];
+            if (std::isupper(ch)) {
+                if (alpha[ch - 'A' + 26] == -1) {
+                    alpha[ch - 'A' + 26] = i;
+                }
+            } else {
+                alpha[ch - 'a'] = i;
+            }
+        }
+        int output = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (alpha[i] != -1 && alpha[i + 26] > alpha[i]) {
+                ++output;
+            }
+        }
+        return output;
+    }
 };
 int main() {
     Solution sol;
@@ -38,6 +60,10 @@ int main() {
     
     int output = sol.numberOfSpecialChars(word);
     assert((expectedOutput == output) && "Test #1 failed!");
+    
+    output = sol.numberOfSpecialCharsWithArray(word);
+    assert((expectedOutput == output) && "Test #2 failed!");
+    
     
     std::cout << "All tests are passed!" << std::endl;
     return 0;
