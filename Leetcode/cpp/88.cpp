d3a136fc52ef8686ca4a7f42785914953a537b7a
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-12 21:50:25 +0300

    Create 88.cpp

---
+++ Leetcode/cpp/88.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int index = nums1.size() - 1;
+        --m;
+        --n;
+        while (m >= 0 || n >= 0) {
+            if (m >= 0 && (n < 0 || nums1[m] > nums2[n])) {
+                nums1[index--] = nums1[m--];
+            } else {
+                nums1[index--] = nums2[n--];
+            }
+        }
+    }
+};
