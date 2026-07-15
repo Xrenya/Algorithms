0c42dd7fe7d22aac01d1ac4de51a5fcf2264b62c
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-15 14:13:27 +0300

    Update 28.cpp

--- Leetcode/cpp/28.cpp
+++ Leetcode/cpp/28.cpp
@@ -1,5 +1,52 @@
 class Solution {
 public:
+    std::vector<int> buildLPS(const std::string& needle) {
+        int n = needle.length();
+        std::vector<int> prefix(n, 0);
+        int index = 1;
+        int length = 0;
+        while (index < n) {
+            if (needle[index] == needle[length]) {
+                prefix[index++] = ++length;
+            } else {
+                if (length != 0) {
+                    length = prefix[length - 1];
+                } else {
+                    prefix[index++] = 0;
+                }
+            }
+        }
+        return prefix;
+    }
+
+    int strStr(std::string haystack, std::string needle) {
+        std::vector<int> lps = buildLPS(needle);
+        int i = 0;
+        int n = haystack.length();
+        int m = needle.length();
+        int j = 0;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                ++i;
+                ++j;
+
+                if (j == m) {
+                    return i - j;
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    ++i;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+class SolutionNotOptimal {
+public:
     int strStr(string haystack, string needle) {
         int m = needle.length();
         int n = haystack.length();
