e7fdc1cd5f271b2422de94cbb8f1064b6e38f9e3
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-15 17:39:08 +0300

    Update 392.cpp

--- Leetcode/cpp/392.cpp
+++ Leetcode/cpp/392.cpp
@@ -10,3 +10,27 @@ public:
         return index == s.size();
     }
 };
+
+
+class SolutionV1 {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.length() > t.length()) {
+            return false;
+        } else if (s.length() == t.length()) {
+            return s == t;
+        } else if (s.length() == 0) {
+            return true;
+        }
+        int index = 0;
+        for (int i = 0; i < t.length(); ++i) {
+            if (s[index] == t[i]) {
+                ++index;
+            }
+            if (index == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
