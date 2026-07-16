0ad26c1e02768bce14d9637cecf9bbe45fd8e9f7
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-16 21:26:58 +0300

    Update 242.cpp

--- Leetcode/cpp/242.cpp
+++ Leetcode/cpp/242.cpp
@@ -1,3 +1,16 @@
+class SolutionV2 {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] != t[i]) return false;
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
