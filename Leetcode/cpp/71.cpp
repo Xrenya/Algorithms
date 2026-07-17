d150bd0d12935f2a578e967c0f2e3c777eebd81e
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-17 09:06:59 +0300

    Update 71.cpp

--- Leetcode/cpp/71.cpp
+++ Leetcode/cpp/71.cpp
@@ -18,3 +18,31 @@ public:
         return simplified_path.empty() ? "/" : simplified_path;
     }
 };
+
+class SolutionV2 {
+public:
+    string simplifyPath(string path) {
+        std::vector<std::string> buffer;
+        std::stringstream ss(path);
+        std::string segment;
+        
+        while (std::getline(ss, segment, '/')) {
+            if (segment == "..") {
+                if (!buffer.empty()) {
+                    buffer.pop_back();
+                }
+            } else if (segment == ".") {
+                continue;
+            } else if (segment != "") {
+                buffer.push_back(segment);
+            }
+        }
+        
+        std::string output;
+        if (buffer.empty()) output += "/";
+        for (const auto c : buffer) {
+            output += "/" + c;
+        }
+        return output;
+    }
+};
