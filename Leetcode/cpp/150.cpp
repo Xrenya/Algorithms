19ce896b26084883a0b00fc4931a6a0f394677b9
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-17 15:10:32 +0300

    Update 150.cpp

--- Leetcode/cpp/150.cpp
+++ Leetcode/cpp/150.cpp
@@ -44,6 +44,53 @@ public:
     }
 };
 
+class SolutionV2 {
+public:
+    enum class Operations {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    };
+    
+    int evalRPN(vector<string>& tokens) {
+        auto ops = [](int a, int b, Operations operation) -> int {
+            switch(operation) {
+                case Operations::PLUS: return a + b;
+                case Operations::MINUS: return a - b;
+                case Operations::MULTIPLY: return a * b;
+                case Operations::DIVIDE: return a / b;
+            }
+        };
+        std::vector<int> stack;
+
+        for (int i = 0; i < tokens.size(); ++i) {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                stack.push_back(std::stoi(tokens[i]));
+            } else {
+                int b = stack.back();
+                stack.pop_back();
+                int a = stack.back();
+                stack.pop_back();
+                Operations current_op = Operations::PLUS;
+                if (tokens[i] == "+") {
+                    current_op = Operations::PLUS;
+                } else if (tokens[i] == "-") {
+                    current_op = Operations::MINUS;
+                } else if (tokens[i] == "*") {
+                    current_op = Operations::MULTIPLY;
+                } else if (tokens[i] == "/") {
+                    current_op = Operations::DIVIDE;
+                }
+                int result = ops(a, b, current_op);
+                
+                stack.push_back(result);
+            }
+        }
+        return stack.back();
+    }
+};
+
 int main() {
   Solution sol;
   std::vector<std::string> tokens = {"2", "1", "+", "3", "*"};
@@ -52,3 +99,5 @@ int main() {
   assert(output == expected_output && "Test #1 failed!");
   std::cout << "Tests are passed!" << std::endl;
 }
+
+
