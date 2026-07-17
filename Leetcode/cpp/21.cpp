91e3d80e3b211f71ed1b9bff3866629039929419
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-17 18:56:09 +0300

    Update 21.cpp

--- Leetcode/cpp/21.cpp
+++ Leetcode/cpp/21.cpp
@@ -38,6 +38,23 @@ public:
     }
 
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        return dfs(list1, list2);
+        if (list1 == nullptr && list2 == nullptr) {
+            return nullptr;
+        } else if (list2 == nullptr && list1 != nullptr) {
+            return list1;
+        } else if (list1 == nullptr && list2 != nullptr) {
+            return list2;
+        }
+        ListNode * ptr;
+        if (list1->val < list2->val) {
+            ptr = list1;
+            list1 = list1->next;
+        } else {
+            ptr = list2;
+            list2 = list2->next;
+        }
+        ptr->next = mergeTwoLists(list1, list2);
+        return ptr;
+        // return dfs(list1, list2);
     }
 };
