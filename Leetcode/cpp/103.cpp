4ba0f59951935205b691dbd69b38e553f192512e
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-18 22:09:15 +0300

    Create 103.cpp

---
+++ Leetcode/cpp/103.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (root == nullptr) {
+            return {};
+        }
+        std::vector<std::vector<int>> output;
+        std::deque<TreeNode*> dq{root};
+        bool reverse = false;
+        while (!dq.empty()) {
+            int size = dq.size();
+            std::vector<int> tmp;
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = dq.front();
+                dq.pop_front();
+                tmp.push_back(node->val);
+                if (node->left) {
+                    dq.push_back(node->left);
+                }
+                if (node->right) {
+                    dq.push_back(node->right);
+                }
+            }
+            if (reverse) {
+                std::reverse(tmp.begin(), tmp.end());
+                reverse = false;
+            } else {
+                reverse = true;
+            }
+            output.push_back(tmp);
+        }
+        return output;
+    }
+};
