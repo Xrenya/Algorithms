fd1d0616b518874e741f4aa5116abefdaeda7600
Rinat <51479797+Xrenya@users.noreply.github.com>
2026-07-19 18:38:51 +0300

    Update 162.cpp

--- Leetcode/cpp/162.cpp
+++ Leetcode/cpp/162.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 #include <cassert>
 
+class Solutionv2 {
+public:
+    int findPeakElement(std::vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+};
+
 
 class Solution {
 public:
